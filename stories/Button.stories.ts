bc844f8396b15ca2a65d8dddb58c2fd1e5b82c46
Iury Nogueira <iunog@hotmail.com>
2022-07-08 14:46:47 -0300

    feat: add storybook :sparkles:

---
+++ stories/Button.stories.ts
@@ -0,0 +1,17 @@
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import { ButtonComponent } from '../projects/ion/src/lib/button/button.component';
+
+export default {
+  title: 'Ion/Buttons',
+  component: ButtonComponent,
+} as Meta;
+
+const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
+  component: ButtonComponent,
+  props: args,
+});
+
+export const CustomLabel = Template.bind({});
+CustomLabel.args = {
+  label: 'Custom label',
+};
