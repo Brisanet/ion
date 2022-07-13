fb57b3461597faf29b2fda51478fe5a4e3037495
Iury Nogueira <iunog@hotmail.com>
2022-07-13 17:34:01 -0300

    refactor: improve tests and icon types #2 :hammer:

--- projects/ion/src/lib/icon/icon.component.spec.ts
+++ projects/ion/src/lib/icon/icon.component.spec.ts
@@ -1,43 +1,45 @@
-import { render, screen } from '@testing-library/angular';
-import { IconType, IonIconComponent, IonIconProps } from './icon.component';
+import { render } from '@testing-library/angular';
+import { IonIconComponent, IonIconProps } from './icon.component';
 
-const sut = async (customProps?: IonIconProps) => {
+const sut = async (customProps?: IonIconProps): Promise<HTMLElement> => {
+  let type = 'trash';
+
   await render(IonIconComponent, {
     componentProperties: customProps || {
-      type: 'pencil',
+      type: 'trash',
     },
   });
+  customProps && (type = customProps.type);
+  return document.getElementById('ion-icon-' + type);
 };
 
 describe('IonIconComponent', () => {
-  it('should render icon with default size', async () => {
-    await sut();
-    const defaultSize = '24';
-    const svgElement = screen.getByTestId('ion-icon-svg');
-    expect(svgElement).toBeTruthy();
-    expect(svgElement).toHaveAttribute('width', defaultSize);
+  it('should render a correct icon', async () => {
+    await sut({ type: 'pencil' });
+    const elementRendered = document.getElementById('ion-icon-pencil');
+    expect(elementRendered).toBeTruthy();
   });
 
-  // it('should render icon with default color', async () => {
-  //   await sut();
-  //   expect(screen.getByTestId('ion-icon-svg')).toHaveAttribute('fill', 'black');
-  // });
+  it('should render icon with default size', async () => {
+    const svgElement = await sut();
+    const defaultSize = '24';
+    expect(svgElement.getAttribute('height')).toBe(defaultSize);
+    expect(svgElement.getAttribute('width')).toBe(defaultSize);
+  });
 
-  // it('should render icon with custom size', async () => {
-  //   const size = 60;
-  //   await sut({ type: 'pencil', size });
-  //   expect(screen.getByTestId('ion-icon-svg')).toHaveAttribute(
-  //     'width',
-  //     `${size}`
-  //   );
-  // });
+  it('should render icon with default color', async () => {
+    expect((await sut()).getAttribute('fill')).toBe('black');
+  });
 
-  // it('should render icon with custom size', async () => {
-  //   const color = 'red';
-  //   await sut({ type: 'pencil', color });
-  //   expect(screen.getByTestId('ion-icon-svg')).toHaveAttribute(
-  //     'fill',
-  //     `${color}`
-  //   );
-  // });
+  it('should render icon with custom size', async () => {
+    const size = 60;
+    const svgElement = await sut({ type: 'trash', size });
+    expect(svgElement.getAttribute('width')).toBe(`${size}`);
+  });
+
+  it('should render icon with custom color', async () => {
+    const color = 'red';
+    const svgElement = await sut({ type: 'trash', color });
+    expect(svgElement.getAttribute('fill')).toBe(`${color}`);
+  });
 });
