1d4d90d8e62e00ea737c19ae5b82d894ba417ada
Danilo Alves <dannalves111@gmail.com>
2022-07-15 14:24:38 -0300

    style: changes to scss

--- projects/ion/src/lib/ion.module.ts
+++ projects/ion/src/lib/ion.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
-import { IonComponent } from './ion.component';
 import { ButtonComponent } from './button/button.component';
+import { IonComponent } from './ion.component';
+import { ThemeComponent } from './theme/theme.component';
 
 @NgModule({
-  declarations: [IonComponent, ButtonComponent],
+  declarations: [IonComponent, ButtonComponent, ThemeComponent],
   imports: [],
   exports: [IonComponent, ButtonComponent],
 })
---
+++ projects/ion/src/lib/theme/theme.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-theme',
+  templateUrl: './theme.component.html',
+  styleUrls: ['./theme.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class ThemeComponent {}
