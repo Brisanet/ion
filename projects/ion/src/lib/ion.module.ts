c18992dad70542e9473458c5e6a023ff4c2d4693
Danilo Alves <dannalves111@gmail.com>
2022-07-15 14:44:44 -0300

    refactor: remove theme component

--- projects/ion/src/lib/ion.module.ts
+++ projects/ion/src/lib/ion.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { ButtonComponent } from './button/button.component';
 import { IonComponent } from './ion.component';
-import { ThemeComponent } from './theme/theme.component';
 
 @NgModule({
-  declarations: [IonComponent, ButtonComponent, ThemeComponent],
+  declarations: [IonComponent, ButtonComponent],
   imports: [],
   exports: [IonComponent, ButtonComponent],
 })
--- projects/ion/src/lib/theme/theme.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-  selector: 'app-theme',
-  templateUrl: './theme.component.html',
-  styleUrls: ['./theme.component.scss'],
-  encapsulation: ViewEncapsulation.None,
-})
-export class ThemeComponent {}
