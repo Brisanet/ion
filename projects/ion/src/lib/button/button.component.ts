cf0aed620bfffd4a99e063d0c871d5ff8766fe05
Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
2022-07-18 17:31:12 -0300

    style: adding styles to various types of buttons

--- projects/ion/src/lib/button/button.component.ts
+++ projects/ion/src/lib/button/button.component.ts
@@ -1,17 +1,21 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 export interface IButton {
   label: string;
-  type: 'primary' | 'danger';
+  type: 'primary' | 'secundary' | 'ghost' | 'dashed' | 'link';
+  size: 'sm' | 'md' | 'lg' | 'xl';
+  load: boolean;
 }
 @Component({
   selector: 'ion-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.less'],
+  styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {
   @Input() label: string;
   @Input() type: IButton['type'] = 'primary';
-
+  @Input() size: IButton['size'] = 'md';
+  @Input() danger = false;
+  @Input() disabled = false;
   @Output() ionOnClick = new EventEmitter();
 
   handleClick() {
