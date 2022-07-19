863a38a5e4eb86c9f177ca91af9031866fa3f1f4
Deeved Hiuston <deeved@gmail.com>
2022-07-18 22:25:25 -0300

    fix: fixes conflict in applying danger class and disabled attribute

--- projects/ion/src/lib/button/button.component.ts
+++ projects/ion/src/lib/button/button.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 export interface IButton {
   label: string;
-  type: 'primary' | 'secundary' | 'ghost' | 'dashed' | 'link';
+  type: 'primary' | 'secundary' | 'ghost' | 'dashed';
   size: 'sm' | 'md' | 'lg' | 'xl';
+  danger: 'true' | 'false';
+  disabled: 'true' | 'false';
   load: boolean;
 }
 @Component({
@@ -10,15 +12,22 @@ export interface IButton {
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
   @Input() label: string;
   @Input() type: IButton['type'] = 'primary';
   @Input() size: IButton['size'] = 'md';
-  @Input() danger = false;
-  @Input() disabled = false;
+  @Input() danger: IButton['danger'] = 'false';
+  @Input() disabled: IButton['disabled'] = 'false';
   @Output() ionOnClick = new EventEmitter();
+  _disabled = false;
+  _danger = false;
 
   handleClick() {
     this.ionOnClick.emit();
+  }
+
+  ngOnInit(): void {
+    this._disabled = this.disabled === 'true' ? true : false;
+    this._danger = this.danger === 'true' ? true : false;
   }
 }
