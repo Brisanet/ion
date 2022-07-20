a3275a48a6f11ab47a3d62b77cf83eeba63c5329
Deeved Hiuston <deeved@gmail.com>
2022-07-19 22:43:02 -0300

    feat: feat: adding expand property on button

--- projects/ion/src/lib/button/button.component.ts
+++ projects/ion/src/lib/button/button.component.ts
@@ -3,6 +3,7 @@ export interface IonButtonProps {
   label: string;
   type: 'primary' | 'secundary' | 'ghost' | 'dashed';
   size: 'sm' | 'md' | 'lg' | 'xl';
+  expand: boolean;
   danger: boolean;
   disabled: boolean;
   load: boolean;
@@ -16,6 +17,7 @@ export class ButtonComponent {
   @Input() label: string;
   @Input() type: IonButtonProps['type'] = 'primary';
   @Input() size: IonButtonProps['size'] = 'md';
+  @Input() expand = false;
   @Input() danger = false;
   @Input() disabled = false;
   @Output() ionOnClick = new EventEmitter();
