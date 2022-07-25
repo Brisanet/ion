3c7cae1a68d7bdbcd3d85458aed11b2dd29609dd
Iury Nogueira <iunog@hotmail.com>
2022-07-25 15:41:26 -0300

    feat: add tests to tab component :sparkles:

--- projects/ion/src/lib/tab/tab.component.spec.ts
+++ projects/ion/src/lib/tab/tab.component.spec.ts
@@ -1,24 +1,53 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { fireEvent, render, screen } from '@testing-library/angular';
+import { TabComponent, TabSize } from './tab.component';
 
-import { TabComponent } from './tab.component';
+const defaultName = 'MinhaTab';
+
+const sut = async (customProps?: {
+  label: string;
+  tabSize?: TabSize;
+  disabled?: boolean;
+}) => {
+  await render(TabComponent, {
+    componentProperties: customProps || {
+      label: defaultName,
+    },
+  });
+};
 
 describe('TabComponent', () => {
-  let component: TabComponent;
-  let fixture: ComponentFixture<TabComponent>;
+  it.each(['about', 'profile'])(
+    'should render label %s',
+    async (tabName: string) => {
+      await sut({ label: tabName });
+      expect(screen.getAllByText(tabName)).toHaveLength(1);
+    }
+  );
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [TabComponent],
-    }).compileComponents();
+  it.each(['md', 'lg'])(
+    'should render with correct size %s',
+    async (size: TabSize) => {
+      await sut({ label: 'Tab', tabSize: size });
+      expect(screen.getByText('Tab')).toHaveClass('tab-' + size);
+    }
+  );
+
+  it.todo('should render with correct border direction');
+
+  it('should click on tab', async () => {
+    await sut();
+    const myTab = screen.getByText(defaultName);
+    fireEvent.click(myTab);
+    expect(myTab).toHaveClass('tab-selected');
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TabComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('should render tab without disabled by default', async () => {
+    await sut();
+    expect(screen.getByText(defaultName)).toBeEnabled();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render tab disabled', async () => {
+    await sut({ label: defaultName, disabled: true });
+    expect(screen.getByText(defaultName)).toBeDisabled();
   });
 });
