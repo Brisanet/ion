929bb220344fe1396edb5fbc2e0b07eab4b76dc2
MarcosOliveir4 <marcosassis071@gmail.com>
2022-07-25 09:00:17 -0300

    fix: update in master

--- projects/ion/src/lib/ion.module.ts
+++ projects/ion/src/lib/ion.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import { ButtonComponent } from './button/button.component';
 import { IonIconComponent } from './icon/icon.component';
 import { IonComponent } from './ion.component';
+import { HeadingComponent } from './typograpry';
 
 @NgModule({
-  declarations: [IonComponent, ButtonComponent, IonIconComponent],
+  declarations: [
+    IonComponent,
+    ButtonComponent,
+    IonIconComponent,
+    HeadingComponent,
+  ],
   imports: [CommonModule],
-  exports: [IonComponent, ButtonComponent, IonIconComponent],
+  exports: [IonComponent, ButtonComponent, IonIconComponent, HeadingComponent],
 })
 export class IonModule {}
