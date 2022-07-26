0af2153776929a1701bbcb8ccf89ca76273925c9
Deeved Hiuston <deeved@gmail.com>
2022-07-26 11:53:33 -0300

    feat: Button Component (#36) :sparkles:
    * feat: add event click and EventEmitter Output Angular
    
    * test: adding default and type button tests
    
    * style: adding styles to various types of buttons
    
    * test: adding tests by type and size
    
    * fix: fixes conflict in applying danger class and disabled attribute
    
    * test: changing structure 'it' to 'describe'
    
    * style: Improved disabled button style and syntax
    
    * refactor: changing Input 'danger' and 'disabled'
    
    * test: changing parameter value from string to boolean
    
    * style: changing color values ​​by theme variables
    
    * style: size adjustment
    
    * feat: feat: adding expand property on button
    
    * style:fix padding
    
    * doc: button documentation
    
    * test: add test of click #35 :test_tube:
    
    * test: add test of click #35 :test_tube:
    
    * style: changing the button's 'focus' to 'focus-visible' and making the requested changes
    
    * test: improving tests
    
    * refactor: improving the interface - creating type: size and type
    
    * docs: remove unnecessary file
    
    * feat: load on button
    
    * test: load on button
    
    * docs: loading button
    
    * docs: improve documentation
    
    * refactor: rename variable
    
    * test: improving tests with it.each
    
    * style: improving the loading button style
    
    * test: improving tests in button
    
    * test: improving test
    
    * style: improving loading button style
    
    * test: improving tests
    
    * Update projects/ion/src/lib/button/button.component.spec.ts
    
    Co-authored-by: Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
    Co-authored-by: Iury Nogueira <iunog@hotmail.com>
    Co-authored-by: Marcos Oliveira <marcosassis071@gmail.com>

--- projects/ion/src/lib/button/button.component.spec.ts
+++ projects/ion/src/lib/button/button.component.spec.ts
@@ -1,5 +1,17 @@
-import { render, screen } from '@testing-library/angular';
-import { ButtonComponent } from './button.component';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { fireEvent, render, screen } from '@testing-library/angular';
+import { ButtonComponent, IonButtonProps } from './button.component';
+
+const defaultName = 'button';
+
+const sut = async (
+  customProps: IonButtonProps = { label: defaultName }
+): Promise<HTMLElement> => {
+  await render(ButtonComponent, {
+    componentProperties: customProps,
+  });
+  return screen.findByRole('button');
+};
 
 describe('ButtonComponent', () => {
   it('should render button with custom label', async () => {
@@ -14,5 +26,81 @@ describe('ButtonComponent', () => {
   it('should render button with warning msg when label is not provided', async () => {
     await render(ButtonComponent);
     expect(screen.getByText('configure uma label'));
+  });
+
+  it('should emit an event when clicked', async () => {
+    const clickEvent = jest.fn();
+    const button = await sut({
+      label: defaultName,
+      ionOnClick: {
+        emit: clickEvent,
+      } as any,
+    });
+    fireEvent.click(button);
+    expect(clickEvent).toHaveBeenCalled();
+  });
+
+  it('should not call event when is loading', async () => {
+    const clickEvent = jest.fn();
+    const button = await sut({
+      label: defaultName,
+      loading: true,
+      ionOnClick: {
+        emit: clickEvent,
+      } as any,
+    });
+    fireEvent.click(button);
+    expect(clickEvent).not.toHaveBeenCalled();
+  });
+});
+
+const types: Array<IonButtonProps['type']> = [
+  'primary',
+  'secondary',
+  'ghost',
+  'dashed',
+];
+
+it.each(types)('should render correct types', async (type) => {
+  expect(await sut({ label: 'button', type })).toHaveClass(`ion-btn-${type}`);
+});
+
+const sizes: Array<IonButtonProps['size']> = ['lg', 'md', 'sm', 'xl'];
+it.each(sizes)('should render correct sizes', async (size) => {
+  expect(await sut({ label: defaultName, size })).toHaveClass(
+    `ion-btn-${size}`
+  );
+});
+
+describe('Danger ButtonComponent', () => {
+  it('should render a button with the danger class when danger="true" is passed', async () => {
+    expect(await sut({ label: defaultName, danger: true })).toHaveClass(
+      'danger'
+    );
+  });
+});
+
+describe('Disabled ButtonComponent', () => {
+  it('should render a disabled button when disabled="true" is passed', async () => {
+    expect(await sut({ label: defaultName, disabled: true })).toHaveAttribute(
+      'disabled'
+    );
+  });
+});
+
+describe('Expand ButtonComponent', () => {
+  it('should render a expand button when expand="true" is passed', async () => {
+    expect((await sut({ label: defaultName, expand: true })).style.width).toBe(
+      '100%'
+    );
+  });
+});
+
+describe('load ButtonComponent', () => {
+  it('should render a loading button when loading="true" is passed', async () => {
+    const button = await sut({ label: defaultName, loading: true });
+    expect(button).toHaveClass('loading');
+    expect(button.children[0]).toHaveClass('spinner');
+    expect(button.children[1].textContent).toContain('Carregando...');
   });
 });
