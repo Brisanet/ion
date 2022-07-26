e1594af8ff94d1f08c7159dedc22f438800203ca
Deeved Hiuston <deeved@gmail.com>
2022-07-26 11:53:33 -0300

    feat: Button Component (#36) :sparkles:
    * feat: add event click and EventEmitter Output Angular
    
    * test: adding default and type button tests
    
    * style: adding styles to various types of buttons
    
    * test: adding tests by type and size
    
    * fix: fixes conflict in applying danger class and disabled attribute
    
    * test: changing structure 'it' to 'describe'
    
    * style: Improved disabled button style and syntax
    
    * refactor: changing Input 'danger' and 'disabled'
    
    * test: changing parameter value from string to boolean
    
    * style: changing color values ​​by theme variables
    
    * style: size adjustment
    
    * feat: feat: adding expand property on button
    
    * style:fix padding
    
    * doc: button documentation
    
    * test: add test of click #35 :test_tube:
    
    * test: add test of click #35 :test_tube:
    
    * style: changing the button's 'focus' to 'focus-visible' and making the requested changes
    
    * test: improving tests
    
    * refactor: improving the interface - creating type: size and type
    
    * docs: remove unnecessary file
    
    * feat: load on button
    
    * test: load on button
    
    * docs: loading button
    
    * docs: improve documentation
    
    * refactor: rename variable
    
    * test: improving tests with it.each
    
    * style: improving the loading button style
    
    * test: improving tests in button
    
    * test: improving test
    
    * style: improving loading button style
    
    * test: improving tests
    
    * Update projects/ion/src/lib/button/button.component.spec.ts
    
    Co-authored-by: Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
    Co-authored-by: Iury Nogueira <iunog@hotmail.com>
    Co-authored-by: Marcos Oliveira <marcosassis071@gmail.com>

--- projects/ion/src/lib/button/button.component.ts
+++ projects/ion/src/lib/button/button.component.ts
@@ -1,10 +1,36 @@
-import { Component, Input } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+type Type = 'primary' | 'secondary' | 'ghost' | 'dashed';
+type Size = 'sm' | 'md' | 'lg' | 'xl';
+export interface IonButtonProps {
+  label?: string;
+  type?: Type;
+  size?: Size;
+  expand?: boolean;
+  danger?: boolean;
+  disabled?: boolean;
+  loading?: boolean;
+  ionOnClick?: EventEmitter<any>;
+}
 @Component({
   selector: 'ion-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {
-  @Input() label: string;
+  @Input() label?: string;
+  @Input() type?: Type = 'primary';
+  @Input() size?: Size = 'md';
+  @Input() expand? = false;
+  @Input() danger? = false;
+  @Input() disabled? = false;
+  @Input() loading? = false;
+  @Output() ionOnClick? = new EventEmitter();
+
+  handleClick() {
+    if (!this.loading) {
+      this.ionOnClick.emit();
+    }
+  }
 }
