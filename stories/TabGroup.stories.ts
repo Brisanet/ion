6714fefed1fb697b21df13495699890456bcea06
Iury Nogueira <iunog@hotmail.com>
2022-08-04 09:26:38 -0300

    ✨ add tab group (#60)
    * feat: add tab group #48
    
    * fix: remove doc :fire:
    
    * Update projects/ion/src/lib/tab-group/tab-group.component.spec.ts
    
    Co-authored-by: Danilo Alves <dannalves111@gmail.com>
    
    * refactor: fixed sizes :art:
    
    * test: fix test case :test_tube:
    
    Co-authored-by: Danilo Alves <dannalves111@gmail.com>

---
+++ stories/TabGroup.stories.ts
@@ -0,0 +1,77 @@
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import { TabGroupComponent } from '../projects/ion/src/lib/tab-group/tab-group.component';
+import { moduleMetadata } from '@storybook/angular';
+import { TabComponent } from '../projects/ion/src/lib/tab/tab.component';
+import { action } from '@storybook/addon-actions';
+
+export default {
+  title: 'Ion/TabGroup',
+  component: TabGroupComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [],
+      declarations: [TabComponent],
+    }),
+  ],
+} as Meta;
+
+// Mock tabs
+const tabs = [];
+for (let index = 1; index <= 8; index++) {
+  tabs.push({
+    name: 'Tab ' + index,
+    selected: false,
+  });
+}
+
+const Template: Story<TabGroupComponent> = (args: TabGroupComponent) => ({
+  component: TabGroupComponent,
+  props: {
+    ...args,
+    selected: action('selected'),
+  },
+});
+
+export const Horizontal = Template.bind({});
+Horizontal.args = {
+  tabs: tabs,
+  selected: action('selected'),
+};
+
+export const Vertical = Template.bind({});
+Vertical.args = {
+  tabs: tabs,
+  alignment: 'vertical',
+  selected: action('selected'),
+};
+
+export const SelectedByDefault = Template.bind({});
+SelectedByDefault.args = {
+  tabs: [
+    {
+      name: 'Selected',
+      selected: true,
+    },
+    {
+      name: 'Not Selected',
+      selected: false,
+    },
+  ],
+  selected: action('selected'),
+};
+
+export const differentSizes = Template.bind({});
+differentSizes.args = {
+  tabs: [
+    {
+      name: 'Selected',
+      selected: true,
+    },
+    {
+      name: 'Not Selected',
+      selected: false,
+    },
+  ],
+  alignment: 'vertical',
+  selected: action('selected'),
+};
