bf4626399d8b5e962dcf0cff99cc718ebca01c49
Iury Nogueira <iunog@hotmail.com>
2022-08-04 11:57:43 -0300

    test: add tests to radio input :test_tube:

--- projects/ion/src/lib/radio/radio.component.spec.ts
+++ projects/ion/src/lib/radio/radio.component.spec.ts
@@ -1,24 +1,55 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { fireEvent, render, screen } from '@testing-library/angular';
 import { RadioComponent } from './radio.component';
 
+const sut = async (customProps = {}) => {
+  await render(RadioComponent, {
+    componentProperties: customProps,
+  });
+  return screen.getByRole('radio');
+};
+
 describe('RadioComponent', () => {
-  let component: RadioComponent;
-  let fixture: ComponentFixture<RadioComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [RadioComponent],
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RadioComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('should create an input', async () => {
+    expect(await sut()).toBeInTheDocument();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create input with label', async () => {
+    const labelText = 'Teste';
+    await sut({ label: labelText });
+    expect(screen.getAllByText(labelText)).toHaveLength(1);
+  });
+
+  describe('Checked/Unchecked', () => {
+    it('should create input unchecked by default', async () => {
+      expect(await sut()).not.toBeChecked();
+    });
+
+    it('should create input checked', async () => {
+      const element = await sut({
+        selected: true,
+      });
+      expect(element).toBeChecked();
+    });
+
+    it('should is marked when clicked', async () => {
+      await sut();
+      const element = screen.getByRole('radio');
+      fireEvent.click(element);
+      expect(element).toBeChecked();
+      expect(element).toHaveClass('radio-selected');
+    });
+  });
+
+  describe('Enabled/Disabled', () => {
+    it('should create input enabled by default', async () => {
+      expect(await sut()).toBeEnabled();
+    });
+
+    it('should create input disabled', async () => {
+      const element = await sut({
+        disabled: true,
+      });
+      expect(element).toBeDisabled();
+    });
   });
 });
