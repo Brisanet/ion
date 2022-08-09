349ad054ce17ae96c9e0e7ec67ad6e00f6020c25
Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
2022-08-09 17:25:58 -0300

    style: adjusting row and column styles

--- projects/ion/src/lib/row/row.component.ts
+++ projects/ion/src/lib/row/row.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ion-row',
-  template: '<ng-content></ng-content>',
+  // template: '<ng-content></ng-content>',
+  templateUrl: './row.component.html',
   styleUrls: ['./row.component.less'],
 })
 export class RowComponent {}
