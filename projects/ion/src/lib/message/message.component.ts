df9038efc1356a46eefd6de6d1c5ba4f167474d5
edsonsnunes <edsonnunes05@icloud.com>
2022-08-09 16:53:30 -0300

    chore: cor icons

--- projects/ion/src/lib/message/message.component.ts
+++ projects/ion/src/lib/message/message.component.ts
@@ -1,5 +1,12 @@
+import { IconType } from './../icon/icon.component';
 import { Component, Input } from '@angular/core';
-
+export type MenssageType =
+  | 'random'
+  | 'positive'
+  | 'negative-1'
+  | 'negative-2'
+  | 'warning'
+  | 'info';
 @Component({
   selector: 'ion-message',
   templateUrl: './message.component.html',
@@ -7,5 +14,6 @@ import { Component, Input } from '@angular/core';
 })
 export class MessageComponent {
   @Input() label!: string;
-  @Input() iconType?: 'pencil';
+  @Input() iconType?: IconType = 'pencil';
+  @Input() messageType?: MenssageType = 'info';
 }
--- stories/Message.stories.ts
+++ stories/Message.stories.ts
@@ -1,9 +1,17 @@
+import { IonIconComponent } from './../projects/ion/src/lib/icon/icon.component';
 import { Story, Meta } from '@storybook/angular/types-6-0';
+import { moduleMetadata } from '@storybook/angular';
 import { MessageComponent } from '../projects/ion/src/lib/message/message.component';
 
 export default {
   title: 'Ion/Message',
   component: MessageComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [],
+      declarations: [IonIconComponent],
+    }),
+  ],
 } as Meta;
 
 const Template: Story<MessageComponent> = (args: MessageComponent) => ({
