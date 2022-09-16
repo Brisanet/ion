fe4b58675f208cccdb3812b24a4912ee3b99db3b
Optiplex 3080 <im.rochamatheus@gmail.com>
2022-09-16 09:39:47 -0300

    feat: add function to disable interdeterminate state

---
+++ projects/ion/src/lib/checkbox/checkbox.component.spec.ts
@@ -0,0 +1,120 @@
+import { SafeAny } from './../utils/safe-any';
+import { render, screen, fireEvent } from '@testing-library/angular';
+import { CheckboxComponent, CheckBoxProps } from './checkbox.component';
+
+const box_id = 'ion-checkbox';
+
+const box_states = {
+  enabled: { state: 'enabled' },
+  checked: { state: 'checked' },
+  indeterminate: { state: 'indeterminate' },
+};
+
+const StateEvents = {
+  enabled: { state: 'checked' },
+  checked: { state: 'unchecked' },
+  indeterminate: { state: 'indeterminate' },
+};
+
+const sut = async (customProps: CheckBoxProps = {}) => {
+  await render(CheckboxComponent, {
+    componentProperties: customProps,
+  });
+};
+
+describe('CehckBoxComponent', () => {
+  describe('component basics', () => {
+    beforeEach(async () => {
+      await sut();
+    });
+    it('should render checkbox', async () => {
+      expect(screen.getByTestId(box_id)).toBeInTheDocument();
+    });
+    it('should render enabled checkbox', async () => {
+      expect(screen.getByTestId(box_id)).toHaveProperty('enabled', true);
+    });
+    it('should render unchecked element', async () => {
+      expect(screen.getByTestId(box_id)).not.toBeChecked();
+    });
+    it('should check when clicked', async () => {
+      const element = screen.getByTestId(box_id);
+      fireEvent.click(element);
+      expect(element).toBeChecked();
+    });
+  });
+  it('should render indeterminate checkbox', async () => {
+    await sut({ state: 'indeterminate' });
+    expect(screen.getByTestId(box_id)).toHaveProperty('indeterminate', true);
+  });
+  it('should render disabled checkbox', async () => {
+    await sut({ disabled: true });
+    expect(screen.getByTestId(box_id)).toBeDisabled();
+  });
+  it.each(['enabled', 'checked', 'indeterminate'])(
+    `should render %s disabled`,
+    async (state) => {
+      await sut({ ...box_states[state], disabled: true });
+      expect(screen.getByTestId(box_id)).toBeDisabled();
+    }
+  );
+  it('should not emit event when disabled', async () => {
+    const clickEvent = jest.fn();
+    await sut({
+      disabled: true,
+      ionClick: {
+        emit: clickEvent,
+      } as SafeAny,
+    });
+    const element = screen.getByTestId(box_id);
+    fireEvent.click(element);
+    expect(clickEvent).not.toHaveBeenCalledWith({ state: 'disabled' });
+  });
+  it('should become unchecked when checked checkbox is clicked', async () => {
+    await sut({ state: 'checked' });
+    const element = screen.getByTestId(box_id);
+    fireEvent.click(element);
+    expect(element).not.toBeChecked();
+  });
+  it('should become enabled when indeterminate checkbox is clicked', async () => {
+    await sut({ state: 'indeterminate' });
+    const element = screen.getByTestId(box_id);
+    fireEvent.click(element);
+    expect(element).not.toBeChecked();
+  });
+
+  it.each(['checked', 'indeterminate'])(
+    'should change checkbox state when state is changed to %s',
+    async (state) => {
+      await sut();
+      const element = screen.getByTestId(box_id);
+      fireEvent.change(element, { target: { state: state } });
+      expect(element).toHaveProperty('state', state);
+    }
+  );
+
+  it.each(['enabled', 'checked'])(
+    'should emit right event when %s is clicked',
+    async (state) => {
+      const clickEvent = jest.fn();
+      await sut({
+        ...box_states[state],
+        ionClick: {
+          emit: clickEvent,
+        } as SafeAny,
+      });
+      const element = screen.getByTestId(box_id);
+      fireEvent.click(element);
+      expect(clickEvent).toHaveBeenLastCalledWith(StateEvents[state]);
+    }
+  );
+  it('should emit indeterminate event', async () => {
+    const clickEvent = jest.fn();
+    await sut({
+      state: 'indeterminate',
+      ionClick: {
+        emit: clickEvent,
+      } as SafeAny,
+    });
+    expect(clickEvent).toBeCalledWith(StateEvents.indeterminate);
+  });
+});
---
+++ projects/ion/src/lib/checkbox/checkbox.component.ts
@@ -0,0 +1,103 @@
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+  OnChanges,
+} from '@angular/core';
+
+export interface CheckBoxProps {
+  disabled?: boolean;
+  state?: CheckBoxStates;
+  ionClick?: EventEmitter<CheckBoxEvent>;
+}
+
+enum CheckBoxEvent {
+  checked = 'checked',
+  enabled = 'unchecked',
+  indeterminate = 'indeterminate',
+}
+
+export type CheckBoxStates = 'checked' | 'enabled' | 'indeterminate';
+
+const stateChange = {
+  enabled: 'checked',
+  checked: 'enabled',
+  indeterminate: 'enabled',
+};
+
+@Component({
+  selector: 'ion-checkbox',
+  templateUrl: './checkbox.component.html',
+  styleUrls: ['./checkbox.component.scss'],
+})
+export class CheckboxComponent implements AfterViewInit, OnChanges {
+  @Input() public state: CheckBoxStates = 'enabled';
+  @Output() public stateChange = new EventEmitter<CheckBoxStates>();
+
+  @Input() public disabled = false;
+  @Output() public disabledChange = new EventEmitter<boolean>();
+
+  @Output() ionClick = new EventEmitter();
+
+  @ViewChild('checkBox', { static: true }) public checkBox: ElementRef;
+
+  setState() {
+    if (this.state === 'indeterminate') this.setIndeterminate();
+    if (this.state === 'checked') this.setChecked();
+    if (this.state === 'enabled') this.setEnabled();
+  }
+
+  changeState() {
+    this.state = stateChange[this.state] as CheckBoxStates;
+    this.setState();
+  }
+
+  clearIndeterminate() {
+    this.checkBox.nativeElement.indeterminate = false;
+  }
+
+  setEnabled() {
+    this.checkBox.nativeElement.checked = false;
+    this.checkBox.nativeElement.enabled = true;
+
+    this.clearIndeterminate();
+    this.emitEvent();
+  }
+
+  setIndeterminate() {
+    this.checkBox.nativeElement.indeterminate = true;
+    this.emitEvent();
+  }
+
+  setChecked() {
+    this.checkBox.nativeElement.checked = true;
+
+    this.clearIndeterminate();
+    this.emitEvent();
+  }
+
+  emitEvent() {
+    this.ionClick.emit({ state: CheckBoxEvent[this.state] });
+  }
+
+  setDisabled() {
+    this.checkBox.nativeElement.disabled = this.disabled;
+  }
+
+  checkState() {
+    this.setState();
+    this.setDisabled();
+  }
+
+  ngAfterViewInit(): void {
+    this.checkState();
+  }
+
+  ngOnChanges() {
+    this.checkState();
+  }
+}
