fefab9318b1b086d442553dd6a79d063eec0eb25
Matheus Rocha <im.rochamatheus@gmail.com>
2022-09-20 16:31:56 -0300

    feat: add mediun and large size stories in tab group

--- stories/TabGroup.stories.ts
+++ stories/TabGroup.stories.ts
@@ -72,6 +72,22 @@ SelectedByDefault.args = {
   selected: action('selected'),
 };
 
+export const mediumSize = Template.bind({});
+mediumSize.args = {
+  tabs,
+  direction: 'vertical',
+  selected: action('selected'),
+  size: 'md',
+};
+
+export const largeSize = Template.bind({});
+largeSize.args = {
+  tabs,
+  direction: 'vertical',
+  selected: action('selected'),
+  size: 'lg',
+};
+
 export const differentSizes = Template.bind({});
 differentSizes.args = {
   tabs: [
