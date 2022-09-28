7edb92cbd74362c808de0225bf5d5d7782af0fa0
Iury Nogueira <iunog@hotmail.com>
2022-09-28 16:06:10 -0300

    feat: add borders in table

--- stories/Table.stories.ts
+++ stories/Table.stories.ts
@@ -154,7 +154,7 @@ WithTagByRow.args = {
 export const WithPagination = Template.bind({});
 WithPagination.args = {
   config: {
-    data,
+    data: [...data, ...data, ...data, ...data],
     columns,
     pagination: {
       total: 46,
