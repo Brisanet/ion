99baaaab9e668bd31bd8d4827f7e38a6aead2a43
Allan Lobo <allanplobo@gmail.com>
2022-10-21 15:47:54 -0300

    :sparkles: Modal (#223)
    * feat: cria base do componente de modal
    
    * feat: adiciona stop propagation para o modal não fazer auto close alem de ajustar UI
    
    * feat: add buttons, icons and logic to emit values
    
    * refactor: remove deprecated keypress
    
    * feat: ion modal service ( logic and stories.ts ) initial preparation to receive params
    
    * feat: start logical props from service to modal component
    
    * feat: start dynamic component implementation on the modal body
    
    * feat: add component dynamically
    
    * feat: add logic to send custom component properties from modal service
    
    * feat: add option to hide footer and prepare a second user case in modal
    
    * feat: start storybook case
    
    * feat: add close by component case in story book
    
    * test: init tests in modal component
    
    * test: finish modal component tests
    
    * test: start modal service tests
    
    * feat: finish service storybook mdx
    
    * feat: finish modal component stories.ts
    
    * test: write modal service tests
    
    * fix: remove unecessary code from HostListener that causes build error
    
    * fix: label, double modal behavior, remove console.log and style/code example
    
    * fix: changed all reviews points
    
    * fix: fix changes accordin to review ( whitespaces, sass variables and test )
    
    * refactor: change modal service name file and add it on public api ts
    
    * refactor: add the importance of the IonModalComponent in entryComponent
    
    * fix: remove settings.json
    
    * refactor: remove the need to inform viewContainerRef, its now appending the modal in the app body
    
    * style: change function position in code
    
    * refactor: remove unecessary code and add destach from appRef
    
    * refactor: adjust response from emitValueAndCloseModal and componentSubscriber
    
    * refactor: change any to safeAny and improve type of observable
    
    * refactor: change close button #162 :hammer:
    
    Co-authored-by: AllanPatrick <allan.patrick@grupobrisanet.com.br>
    Co-authored-by: Iury Nogueira <iunog@hotmail.com>

---
+++ projects/ion/src/lib/modal/modal.service.ts
@@ -0,0 +1,79 @@
+import { SafeAny } from './../utils/safe-any';
+import { DOCUMENT } from '@angular/common';
+import {
+  ApplicationRef,
+  ComponentFactoryResolver,
+  ComponentRef,
+  Inject,
+  Injectable,
+  Injector,
+  Type,
+} from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+import { IonModalComponent } from './component/modal.component';
+import {
+  IonModalConfiguration,
+  IonModalResponse,
+} from './models/modal.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IonModalService {
+  private modalComponentRef!: ComponentRef<IonModalComponent>;
+  private componentSubscriber!: Subject<IonModalResponse | unknown>;
+
+  constructor(
+    // TODO: SafeAny used due to an issue in Angular 8 (https://github.com/angular/angular/issues/20351). When projects are updated to v9, change "SafeAny" to "Document";
+    @Inject(DOCUMENT) private document: SafeAny,
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private appRef: ApplicationRef,
+    private injector: Injector
+  ) {}
+
+  open(
+    component: Type<unknown>,
+    configuration?: IonModalConfiguration
+  ): Observable<IonModalResponse | unknown> {
+    const modal = this.componentFactoryResolver
+      .resolveComponentFactory(IonModalComponent)
+      .create(this.injector);
+
+    this.modalComponentRef = modal;
+    this.modalComponentRef.instance.componentToBody = component;
+    this.modalComponentRef.instance.setDefaultConfig();
+    if (configuration) {
+      this.modalComponentRef.instance.setConfig(configuration);
+    }
+    this.appRef.attachView(this.modalComponentRef.hostView);
+    this.modalComponentRef.changeDetectorRef.detectChanges();
+
+    const modalElement = this.modalComponentRef.location.nativeElement;
+    this.document.body.appendChild(modalElement);
+
+    this.modalComponentRef.instance.ionOnClose.subscribe(
+      (valueFromModal: IonModalResponse) => {
+        if (!valueFromModal) {
+          this.closeModal();
+          return;
+        }
+
+        this.emitValueAndCloseModal(valueFromModal);
+      }
+    );
+    this.componentSubscriber = new Subject<IonModalResponse | unknown>();
+    return this.componentSubscriber.asObservable();
+  }
+
+  emitValueAndCloseModal(valueToEmit: IonModalResponse | unknown) {
+    this.componentSubscriber.next(valueToEmit);
+    this.closeModal();
+  }
+
+  closeModal() {
+    this.appRef.detachView(this.modalComponentRef.hostView);
+    this.componentSubscriber.complete();
+    this.modalComponentRef.destroy();
+  }
+}
