9bdcfb766ed6c81cd53cabe5ac41e0047a4a1e25
dayannebugarim <dayannebugarim1@gmail.com>
2022-11-29 10:56:22 -0300

    fix: fixing wrong import(#176)

--- projects/ion/src/lib/avatar/avatar.module.ts
+++ projects/ion/src/lib/avatar/avatar.module.ts
@@ -1,10 +1,11 @@
-import { AvatarComponent } from './../../../../../storybook-static/lib/avatar/avatar.component';
+import { AvatarComponent } from './avatar.component';
 import { IonIconComponent } from './../icon/icon.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DefaultImageDirective } from '../defaultImage.directive';
 
 @NgModule({
-  declarations: [AvatarComponent, IonIconComponent],
+  declarations: [AvatarComponent, IonIconComponent, DefaultImageDirective],
   imports: [CommonModule],
   exports: [AvatarComponent, IonIconComponent],
 })
