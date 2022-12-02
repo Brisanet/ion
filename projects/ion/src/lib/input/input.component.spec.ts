8a91c9eb2beee3709bb425655d00d0183c9f7540
Edson Nunes <87482198+edsonsnunes@users.noreply.github.com>
2022-12-02 15:00:50 -0300

    :sparkles: add button option in input (#297)
    * feat: primeiro-commit
    
    * feat: input
    
    * feat: add ion icon
    
    * feat: add icon sub
    
    * feat: add tests
    
    * fix: ajuste na importacao do modulo
    
    * feat: nomeclatura
    
    * fix: test to input counter
    
    * feat: test increment/decrement
    
    * feat: teste decrement 0
    
    * refactor: using ion-button
    
    * feat: changing at all input-counter
    
    * feat: nomeclatura
    
    * feat: ionModule
    
    * feat: add function eventEmit
    
    * feat: add test
    
    * feat: change in test
    
    * feat: placeholder optional
    
    * feat: add test placeholder
    
    * feat: rename test
    
    * feat: cursor pointer removed
    
    Co-authored-by: MarcosOliveir4 <marcosassis071@gmail.com>
    Co-authored-by: Iury Nogueira <iunog@hotmail.com>
    Co-authored-by: mizek1 <dannalves111@gmail.com>

--- projects/ion/src/lib/input/input.component.spec.ts
+++ projects/ion/src/lib/input/input.component.spec.ts
@@ -1,15 +1,16 @@
+import { SafeAny } from './../utils/safe-any';
+import { CommonModule } from '@angular/common';
 import { IonIconComponent } from './../icon/icon.component';
 import { render, screen, fireEvent } from '@testing-library/angular';
 import userEvent from '@testing-library/user-event';
 import { InputComponent, IonInputProps } from './input.component';
 import { FormsModule } from '@angular/forms';
-import { SafeAny } from '../utils/safe-any';
 
 const sut = async (customProps?: IonInputProps): Promise<void> => {
   await render(InputComponent, {
     componentProperties: customProps,
+    imports: [CommonModule, FormsModule],
     declarations: [IonIconComponent],
-    imports: [FormsModule],
   });
 };
 
@@ -17,15 +18,28 @@ describe('InputComponent', () => {
   it('Should allow letters to be inputted', async () => {
     await sut();
     const inputValue = 'input';
-    fireEvent.change(screen.getByTestId('inputElement'), {
+    fireEvent.change(screen.getByTestId('input-element'), {
       target: { value: inputValue },
     });
-    expect(screen.getByTestId('inputElement')).toHaveValue(inputValue);
+    expect(screen.getByTestId('input-element')).toHaveValue(inputValue);
+  });
+
+  it('should render input with an empty placeholder if none is passed', async () => {
+    await sut();
+    const input = screen.getByTestId('input-element');
+    expect(input).not.toHaveAttribute('placeholder');
+  });
+
+  it('should render input with a given placeholder', async () => {
+    const placeholder = 'Search';
+    await sut({ placeholder });
+    const input = screen.getByTestId('input-element');
+    expect(input).toHaveAttribute('placeholder', placeholder);
   });
 
   it('should render input component disabled', async () => {
     await sut({ disabled: true });
-    const element = screen.getByTestId('inputElement');
+    const element = screen.getByTestId('input-element');
     expect(element).toBeDisabled();
   });
 
@@ -41,7 +55,43 @@ describe('InputComponent', () => {
     expect(document.getElementById('ion-icon-' + icon)).toBeTruthy();
   });
 
-  //Falta condição de quando o input vai ser valido e invalido
+  it('should render button when informed', async () => {
+    await sut({ inputButton: true });
+    const button = screen.getByTestId('input-button');
+    fireEvent.click(button);
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should render input icon button when informed', async () => {
+    await sut({ inputIconButton: true });
+    const button = screen.getByTestId('inputIcon-button');
+    fireEvent.click(button);
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should emit an event when clicked input button', async () => {
+    const clickEvent = jest.fn();
+    await sut({
+      inputButton: true,
+      clickButton: {
+        emit: clickEvent,
+      } as SafeAny,
+    });
+    fireEvent.click(screen.getByTestId('input-button'));
+    expect(clickEvent).toHaveBeenCalled();
+  });
+
+  it('should emit an event when clicked input icon button', async () => {
+    const clickEvent = jest.fn();
+    await sut({
+      inputIconButton: true,
+      clickButton: {
+        emit: clickEvent,
+      } as SafeAny,
+    });
+    fireEvent.click(screen.getByTestId('inputIcon-button'));
+    expect(clickEvent).toHaveBeenCalled();
+  });
 
   it.skip('should render input icon valid', async () => {
     await sut();
@@ -66,17 +116,17 @@ describe('InputComponent', () => {
     });
 
     it('should change value when something is typed on input', async () => {
-      userEvent.type(screen.getByTestId('inputElement'), value);
-      expect(screen.getByTestId('inputElement')).toHaveValue(value);
+      userEvent.type(screen.getByTestId('input-element'), value);
+      expect(screen.getByTestId('input-element')).toHaveValue(value);
     });
 
     it('should emit valueChange everytime a key is typed on input', async () => {
-      userEvent.type(screen.getByTestId('inputElement'), value);
+      userEvent.type(screen.getByTestId('input-element'), value);
       expect(mockFn).toHaveBeenCalledTimes(value.length);
     });
 
     it('should emit the value on the last emit', async () => {
-      userEvent.type(screen.getByTestId('inputElement'), value);
+      userEvent.type(screen.getByTestId('input-element'), value);
       expect(mockFn).toHaveBeenLastCalledWith(value);
     });
   });
