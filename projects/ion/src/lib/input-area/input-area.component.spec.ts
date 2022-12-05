50108094943869fc863ca4dd6210cf997d900aea
edsonsnunes <edsonnunes05@icloud.com>
2022-12-05 11:50:17 -0300

    feat: add hover/focus/active

--- projects/ion/src/lib/input-area/input-area.component.spec.ts
+++ projects/ion/src/lib/input-area/input-area.component.spec.ts
@@ -11,14 +11,14 @@ const sut = async (customProps?: IonInputAreaProps): Promise<void> => {
 };
 
 describe('InputAreaCompoenent', () => {
-  it('', async () => {
+  it('should render input with a given placeholder', async () => {
     const placeholder = 'Search';
     await sut({ placeholder });
     const input = screen.getByTestId('input-area');
     expect(input).toHaveAttribute('placeholder', placeholder);
   });
 
-  it('', async () => {
+  it('should render input with an empty placeholder if none is passed', async () => {
     await sut();
     const input = screen.getByTestId('input-area');
     expect(input).not.toHaveAttribute('placeholder');
