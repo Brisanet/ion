146438ba258b74050b96c249f8a925221bc94fe1
Iury Nogueira <iunog@hotmail.com>
2022-12-07 09:12:28 -0300

    🚑 fix pagination remount (#355)
    * feat: branch to test table
    
    * fix: correctly import of ConfigSmartTable
    
    * fix: pagination remount
    
    * fix: pagination remount
    
    * fix: imports msg stories
    
    * refactor: detect changes in pagination :hammer:
    
    Co-authored-by: Alysson Keysson <alysson.mascarenhas@grupobrisanet.com.br>

--- projects/ion/src/lib/pagination/pagination.component.ts
+++ projects/ion/src/lib/pagination/pagination.component.ts
@@ -1,5 +1,13 @@
 import { DropdownItem } from './../dropdown/dropdown.component';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 
 interface Page {
   page_number: number;
@@ -28,7 +36,7 @@ export const ITEMS_PER_PAGE_DEFAULT = 10;
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.scss'],
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent implements OnChanges {
   @Input() total: IonPaginationProps['total'];
   @Input() itemsPerPage: IonPaginationProps['itemsPerPage'] =
     ITEMS_PER_PAGE_DEFAULT;
@@ -52,9 +60,10 @@ export class PaginationComponent implements OnInit {
     this.remountPages();
   }
 
-  ngOnInit(): void {
-    this.createPages(this.totalPages());
-    this.selectPage(1);
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.total) {
+      this.remountPages();
+    }
   }
 
   selectPage(pageNumber: number): void {
@@ -95,7 +104,6 @@ export class PaginationComponent implements OnInit {
   }
 
   remountPages(): void {
-    this.pages = [];
     this.createPages(this.totalPages());
     this.selectPage(1);
   }
@@ -106,6 +114,7 @@ export class PaginationComponent implements OnInit {
   }
 
   private createPages(qtdOfPages: number): void {
+    this.pages = [];
     for (let index = 0; index < qtdOfPages; index++) {
       this.pages.push({
         selected: false,
