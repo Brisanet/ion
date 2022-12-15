3af042ccd96d8e0c766eb1c2191fc0a7d33b610d
mizek1 <dannalves111@gmail.com>
2022-12-15 11:59:00 -0300

    refactor: tooltip tests

--- projects/ion/src/lib/tooltip/tooltip.directive.spec.ts
+++ projects/ion/src/lib/tooltip/tooltip.directive.spec.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { fireEvent, render, screen } from '@testing-library/angular';
-import { TooltipColorScheme } from '../core/types';
+import { TooltipColorScheme, TooltipPosition } from '../core/types';
 import { TooltipModule } from './tooltip.module';
 
 @Component({
   template: `
-    <p data-testid="hostTooltip" ionTooltip [ionTooltipTitle]="ionTooltipTitle">
+    <p
+      data-testid="hostTooltip"
+      ionTooltip
+      [ionTooltipTitle]="ionTooltipTitle"
+      [ionTooltipColorScheme]="ionTooltipColorScheme"
+      [ionTooltipPosition]="ionTooltipPosition"
+    >
       Hover me
     </p>
   `,
@@ -14,6 +20,7 @@ import { TooltipModule } from './tooltip.module';
 class HostTestComponent {
   ionTooltipTitle = 'Tooltip';
   ionTooltipColorScheme: TooltipColorScheme = 'dark';
+  ionTooltipPosition: TooltipPosition = TooltipPosition.DEFAULT;
 }
 
 const sut = async (props: Partial<HostTestComponent> = {}): Promise<void> => {
@@ -24,18 +31,57 @@ const sut = async (props: Partial<HostTestComponent> = {}): Promise<void> => {
 };
 
 describe('Directive: Tooltip', () => {
-  beforeEach(async () => {
-    await sut();
+  afterEach(async () => {
+    fireEvent.mouseLeave(screen.getByTestId('hostTooltip'));
   });
 
   it('should render without tooltip', async () => {
+    await sut();
     expect(screen.queryByTestId('ion-tooltip')).not.toBeInTheDocument();
   });
+
+  it('should create tooltip', async () => {
+    await sut();
+    fireEvent.mouseEnter(screen.getByTestId('hostTooltip'));
+    expect(screen.getByTestId('ion-tooltip')).toBeInTheDocument();
+  });
+
   it('should create tooltip and destroy tooltip', async () => {
+    await sut();
     fireEvent.mouseEnter(screen.getByTestId('hostTooltip'));
     expect(screen.getByTestId('ion-tooltip')).toBeInTheDocument();
 
     fireEvent.mouseLeave(screen.getByTestId('hostTooltip'));
     expect(screen.queryByTestId('ion-tooltip')).not.toBeInTheDocument();
   });
+
+  it('should render tooltip with correct title', async () => {
+    const ionTooltipTitle = 'Hello world!';
+    await sut({ ionTooltipTitle });
+
+    fireEvent.mouseEnter(screen.getByTestId('hostTooltip'));
+    expect(screen.getByText(ionTooltipTitle)).toBeInTheDocument();
+  });
+
+  it.each(['light', 'dark'] as TooltipColorScheme[])(
+    'should render tooltip with %s color scheme',
+    async (ionTooltipColorScheme) => {
+      await sut({ ionTooltipColorScheme });
+      fireEvent.mouseEnter(screen.getByTestId('hostTooltip'));
+      expect(screen.getByTestId('ion-tooltip')).toHaveClass(
+        `ion-tooltip-${ionTooltipColorScheme}`
+      );
+    }
+  );
+
+  it.each(Object.values(TooltipPosition))(
+    'should render tooltip on %s position',
+    async (ionTooltipPosition) => {
+      await sut({ ionTooltipPosition });
+      fireEvent.mouseEnter(screen.getByTestId('hostTooltip'));
+      expect(screen.getByTestId('ion-tooltip')).toHaveClass(
+        `ion-tooltip-position--${ionTooltipPosition}`
+      );
+    }
+  );
 });
