49b83e751fe6d495717d0d3afe88016e02beac61
Edson Nunes <87482198+edsonsnunes@users.noreply.github.com>
2023-01-03 11:19:14 -0300

    :sparkles: Input area (#339)
    * fix: chip badge center
    
    * feat: padding badge component
    
    * feat: change padding/badge
    
    * feat: span in ion-badge
    
    * feat: input-area
    
    * feat: add hover/focus/active
    
    * feat: add test columns/rows
    
    * feat: add input value
    
    * feat: test input value
    
    * feat: test userEvent
    
    * feat: add disabled
    
    * feat: test emit event
    
    * feat: add input area module
    
    Co-authored-by: Iury Nogueira <iunog@hotmail.com>

---
+++ projects/ion/src/lib/input-area/input-area.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+export interface IonInputAreaProps {
+  cols?: string;
+  rows?: string;
+  disabled?: boolean;
+  value?: string;
+  placeholder?: string;
+  valueChange?: EventEmitter<string>;
+}
+
+@Component({
+  selector: 'ion-input-area',
+  templateUrl: './input-area.component.html',
+  styleUrls: ['./input-area.component.scss'],
+})
+export class InputAreaComponent {
+  @Input() cols = '30';
+  @Input() rows = '5';
+  @Input() disabled = false;
+  @Input() value: string;
+  @Input() placeholder?: string;
+  @Output() valueChange = new EventEmitter<string>();
+
+  onChange(value: string): void {
+    this.valueChange.emit(value);
+  }
+}
