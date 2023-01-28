0d7c4aaea0705058b3972c90abf7dd9a8b413a89
Deeved Hiuston <deeved@gmail.com>
2023-01-28 19:52:41 -0300

    test: adding test for DatePickerInputComponent

--- projects/ion/src/lib/picker/date-picker/date-picker-input/date-picker-input.component.spec.ts
+++ projects/ion/src/lib/picker/date-picker/date-picker-input/date-picker-input.component.spec.ts
@@ -1,5 +1,53 @@
-describe('DatePickerInput', () => {
-  it('Should', () => {
-    expect(true).toBeTruthy();
+import { render, screen, fireEvent } from '@testing-library/angular';
+import {
+  DatePickerInputComponent,
+  DatePickerInputComponentProps,
+} from './date-picker-input.component';
+import { ButtonModule } from '../../../button/button.module';
+
+const sut = async (
+  customProps?: DatePickerInputComponentProps
+): Promise<void> => {
+  await render(DatePickerInputComponent, {
+    componentProperties: customProps,
+    imports: [ButtonModule],
+    declarations: [],
+  });
+};
+
+describe('DatePickerInputComponent', () => {
+  it('Should render DatePickerInputComponent', async () => {
+    await sut();
+    const input = screen.getByTestId('input-element');
+    expect(screen.findByTestId('container-input')).toBeTruthy();
+    expect(input).toHaveValue('');
+    expect(input).toHaveAttribute('placeholder', 'Selecione a data');
+  });
+
+  it('should render "2023-01-28"', async () => {
+    const date = '2023-01-28';
+    await sut({ date });
+    expect(screen.getByTestId('input-element')).toHaveValue(date);
+  });
+
+  it('Should render "placeholder render" in placeholder input', async () => {
+    const placeholder = 'placeholder render';
+    await sut({ placeholder });
+    const input = screen.getByTestId('input-element');
+    expect(input).toHaveAttribute('placeholder', placeholder);
+  });
+
+  it('should clear input when clicking in close button', async () => {
+    const date = '2023-01-28';
+    await sut({ date });
+    let input = await screen.getByTestId('input-element');
+    expect(input).toHaveValue(date);
+
+    const clearButton = await screen.findByTestId('inputIcon-button');
+    expect(clearButton).toBeTruthy();
+
+    fireEvent.click(clearButton);
+    input = await screen.getByTestId('input-element');
+    expect(input).toHaveValue('');
   });
 });
--- projects/ion/src/lib/picker/date-picker/date-picker-input/date-picker-input.component.ts
+++ projects/ion/src/lib/picker/date-picker/date-picker-input/date-picker-input.component.ts
@@ -1,13 +1,19 @@
+import { SafeAny } from './../../../utils/safe-any';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+export interface DatePickerInputComponentProps {
+  date?: string;
+  placeholder?: string;
+  clearDate?: EventEmitter<SafeAny>;
+}
 @Component({
   selector: 'date-picker-input',
   templateUrl: './date-picker-input.component.html',
   styleUrls: ['./date-picker-input.component.scss'],
 })
 export class DatePickerInputComponent {
-  @Input() date = '';
-  @Input() placeholder = 'Selecione a data';
+  @Input() date?: string;
+  @Input() placeholder? = 'Selecione a data';
   @Output() clearDate = new EventEmitter();
 
   clearDateValue(): void {
