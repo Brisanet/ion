0db1d9ff59805b93bc3dc3281ec49e6861934e0e
Deeved Hiuston <deeved@gmail.com>
2023-01-30 19:28:27 -0300

    docs: refactor datepicker storybook

--- stories/Datepicker.stories.ts
+++ stories/Datepicker.stories.ts
@@ -1,27 +1,37 @@
-import { CommonModule } from '@angular/common';
 import { Meta, Story } from '@storybook/angular/types-6-0';
 import { action } from '@storybook/addon-actions';
+import {
+  DatepickerComponent,
+  DatePickerComponentProps,
+} from './../projects/ion/src/lib/picker/date-picker/date-picker.component';
+import { DatePickerCalendarComponent } from './../projects/ion/src/lib/picker/date-picker/date-picker-calendar/date-picker-calendar.component';
+import { DatePickerInputComponent } from './../projects/ion/src/lib/picker/date-picker/date-picker-input/date-picker-input.component';
+import { ControlPickerComponent } from './../projects/ion/src/lib/picker/control-picker/control-picker.component';
 import { IonDividerComponent } from '../projects/ion/src/public-api';
-import { DatePicker } from '../projects/ion/src/lib/picker/date-picker/date-picker-calendar/date-picker-calendar.component';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { ButtonModule } from '../projects/ion/src/lib/button/button.module';
 
 export default {
   title: 'Ion/Data Entry/Datepicker',
-  component: DatePicker,
+  component: DatepickerComponent,
 } as Meta;
 
-const Template: Story<DatePicker> = (args: DatePicker) => ({
-  component: DatePicker,
+const Template: Story<DatepickerComponent> = (
+  args: DatePickerComponentProps
+) => ({
+  component: DatepickerComponent,
   props: { ...args, ionOnClick: action('ionOnClick') },
   moduleMetadata: {
-    declarations: [IonDividerComponent],
-    imports: [CommonModule, FormsModule, ButtonModule],
+    declarations: [
+      DatepickerComponent,
+      ControlPickerComponent,
+      DatePickerInputComponent,
+      DatePickerCalendarComponent,
+      IonDividerComponent,
+    ],
+    imports: [CommonModule, ButtonModule],
   },
 });
 
 export const Basic = Template.bind({});
-Basic.args = {
-  initialDate: '2022-10-28',
-  lang: 'pt-BR',
-};
+Basic.args = {};
