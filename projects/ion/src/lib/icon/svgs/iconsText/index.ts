522b202e552c990e3c9d65d249db9e1ebead894a
Vinícius Guedes <comercial.viniciusguedes@gmail.com>
2023-02-02 14:17:07 -0300

    feat: add wait warning icon (#437) :art:
    * feat: add headset icon
    
    * fix: replace headset icon
    
    * fix: replace headset icon
    
    * feat: adding wait-warning icon

--- projects/ion/src/lib/icon/svgs/iconsText/index.ts
+++ projects/ion/src/lib/icon/svgs/iconsText/index.ts
@@ -61,5 +61,6 @@ export { default as working } from './working';
 export { default as arrowsLeftRight } from './arrows-left-right';
 export { default as onu } from './onu';
 export { default as headset } from './headset';
+export { default as waitWarning } from './wait-warning';
 export { default as download } from './download';
 export { default as upload } from './upload';
