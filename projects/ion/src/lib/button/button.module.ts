4a416459d8fcd7d8d091d4055944ed77829528b8
mizek1 <dannalves111@gmail.com>
2023-02-03 08:53:10 -0300

    refactor: add button module (partial)

--- projects/ion/src/lib/button/button.module.ts
+++ projects/ion/src/lib/button/button.module.ts
@@ -1,27 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonButtonComponent } from './button.component';
-import { IonIconComponent } from '../icon/icon.component';
-import { IonDropdownComponent } from '../dropdown/dropdown.component';
-import { IonBadgeComponent } from '../badge/badge.component';
-import { IonInputComponent } from '../input/input.component';
-import { FormsModule } from '@angular/forms';
+import { IonSharedModule } from '../shared.module';
 
 @NgModule({
-  declarations: [
-    IonButtonComponent,
-    IonIconComponent,
-    IonDropdownComponent,
-    IonBadgeComponent,
-    IonInputComponent,
-  ],
-  exports: [
-    IonButtonComponent,
-    IonIconComponent,
-    IonBadgeComponent,
-    IonDropdownComponent,
-    IonInputComponent,
-  ],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, IonSharedModule],
+  exports: [IonButtonComponent],
 })
-export class ButtonModule {}
+export class IonButtonModule {}
