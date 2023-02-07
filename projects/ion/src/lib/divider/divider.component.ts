5f5705a9d27354fd4fc406659fedc1102ea4b562
mizek1 <dannalves111@gmail.com>
2023-02-07 13:12:25 -0300

    refactor: divider types

--- projects/ion/src/lib/divider/divider.component.ts
+++ projects/ion/src/lib/divider/divider.component.ts
@@ -1,15 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { DirectionType } from '../core/types';
-
-type DividerType = 'solid' | 'dashed' | 'text';
-
-export interface IonDividerProps {
-  label?: string;
-  direction?: DirectionType;
-  type?: DividerType;
-  margin?: boolean;
-}
-
+import { DividerType } from '../core/types/divider';
 @Component({
   selector: 'ion-divider',
   templateUrl: './divider.component.html',
