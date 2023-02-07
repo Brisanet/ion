44748e7fbca56835db3f559ded6e26e9eb1e47cd
mizek1 <dannalves111@gmail.com>
2023-02-07 14:59:07 -0300

    refactor: organize imports

--- projects/ion/src/lib/shared.module.ts
+++ projects/ion/src/lib/shared.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonBadgeComponent } from './badge/badge.component';
 import { IonButtonComponent } from './button/button.component';
 import { IonDropdownComponent } from './dropdown/dropdown.component';
 import { IonIconComponent } from './icon/icon.component';
-import { IonBadgeComponent } from './badge/badge.component';
 import { IonInputComponent } from './input/input.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
