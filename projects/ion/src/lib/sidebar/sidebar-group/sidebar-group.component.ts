121a9810f438b1232ccfb269e332513822c4b7ad
mizek1 <dannalves111@gmail.com>
2023-02-16 11:05:02 -0300

    refactor: sidebar group

--- projects/ion/src/lib/sidebar/sidebar-group/sidebar-group.component.ts
+++ projects/ion/src/lib/sidebar/sidebar-group/sidebar-group.component.ts
@@ -25,11 +25,9 @@ export class IonSidebarGroupComponent implements OnChanges {
 
   public itemSelected(itemIndex: number): void {
     this.selected = true;
-    this.items.forEach((item) => (item.selected = false));
-    this.items[itemIndex].selected = true;
-    if (this.items[itemIndex].action) {
-      this.items[itemIndex].action();
-    }
+    this.unselectAllItems();
+    this.selectItem(itemIndex);
+    this.callItemAction(itemIndex);
     this.atClick.emit();
   }
 
@@ -38,12 +36,28 @@ export class IonSidebarGroupComponent implements OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (
-      !changes.firstChange &&
-      changes.selected &&
-      !changes.selected.currentValue
-    ) {
-      this.items.forEach((item) => (item.selected = false));
+    if (this.selectedChangedToFalse(changes)) {
+      this.unselectAllItems();
+    }
+  }
+
+  private selectedChangedToFalse(changes: SimpleChanges): boolean {
+    return (
+      !changes.firstChange && changes.selected && !changes.selected.currentValue
+    );
+  }
+
+  private unselectAllItems(): void {
+    this.items.forEach((item) => (item.selected = false));
+  }
+
+  private selectItem(index: number): void {
+    this.items[index].selected = true;
+  }
+
+  private callItemAction(index: number): void {
+    if (this.items[index].action) {
+      this.items[index].action();
     }
   }
 }
