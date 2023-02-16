91521caaddae2e12f7777abcdaad5285593e5455
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2023-02-16 11:51:09 -0300

    refactor: ordering naming of arrow position classes

--- projects/ion/src/lib/popover/popover.module.ts
+++ projects/ion/src/lib/popover/popover.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonPopoverComponent } from './popover.component';
 import { PopoverDirective } from './popover.directive';
+import { IonDividerModule } from '../divider/divider.module';
+import { IonButtonModule } from '../button/button.module';
 
 @NgModule({
   declarations: [IonPopoverComponent, PopoverDirective],
-  imports: [CommonModule],
+  imports: [CommonModule, IonDividerModule, IonButtonModule],
   exports: [PopoverDirective],
   entryComponents: [IonPopoverComponent],
 })
