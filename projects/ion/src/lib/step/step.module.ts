261590c23ec431db119f6b1cb4905828cbd5a486
Wérmeson Lopes <wermesonsl@outlook.com>
2023-02-22 17:11:05 -0300

    feat: added ion icon module

--- projects/ion/src/lib/step/step.module.ts
+++ projects/ion/src/lib/step/step.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonStepsComponent } from './step.component';
-import { IonModule } from '../ion.module';
+import { IonIconModule } from '../icon/icon.module';
 
 @NgModule({
   declarations: [IonStepsComponent],
-  imports: [CommonModule, IonModule],
+  imports: [CommonModule, IonIconModule],
   exports: [IonStepsComponent],
 })
 export class IonStepsModule {}
