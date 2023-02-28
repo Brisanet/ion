d7c82e79d5d2bb26c8ce2be3945a3849e263c542
mizek1 <dannalves111@gmail.com>
2023-02-28 10:05:25 -0300

    feat: radio group

--- stories/Radio.stories.ts
+++ stories/Radio.stories.ts
@@ -1,5 +1,6 @@
 import { Story, Meta } from '@storybook/angular/types-6-0';
 import { IonRadioComponent } from '../projects/ion/src/lib/radio/radio.component';
+import { IonRadioGroupComponent } from '../projects/ion/src/lib/radio-group/radio-group.component';
 
 export default {
   title: 'Ion/Data Entry/Radio',
@@ -27,3 +28,28 @@ checked.args = {
   label: 'Checked',
   checked: true,
 };
+
+export const RadioGroup = () => ({
+  component: IonRadioGroupComponent,
+  props: {
+    options: [
+      {
+        label: 'Option 1',
+        value: 'option-1',
+      },
+      {
+        label: 'Option 2',
+        value: 'option-2',
+      },
+      {
+        label: 'Option 3',
+        value: 'option-3',
+      },
+      {
+        label: 'Option 4',
+        value: 'option-4',
+        disabled: true,
+      },
+    ],
+  },
+});
