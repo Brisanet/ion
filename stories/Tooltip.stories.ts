d225bcf65b2d26431bdbaf4a3a69c59c3087532e
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2023-03-02 09:02:03 -0300

    fix: fix wrong import in tooltipe story

--- stories/Tooltip.stories.ts
+++ stories/Tooltip.stories.ts
@@ -4,10 +4,10 @@ import { Meta, Story } from '@storybook/angular/types-6-0';
 import {
   TooltipPosition,
   TooltipTrigger,
+  TooltipProps,
 } from '../projects/ion/src/lib/core/types';
 import { IonTooltipComponent } from '../projects/ion/src/lib/tooltip/tooltip.component';
 import { IonTooltipDirective } from '../projects/ion/src/lib/tooltip/tooltip.directive';
-import { TooltipProps } from '../projects/ion/src/lib/core/types/tooltip';
 
 export default {
   title: 'Ion/Data Display/Tooltip',
