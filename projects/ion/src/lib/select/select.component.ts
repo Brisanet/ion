0d220ca2c0931012900b456dd59eafc4090f77af
Deeved Hiuston <deeved@gmail.com>
2023-04-04 19:13:00 -0300

    feat: options visibility

--- projects/ion/src/lib/select/select.component.ts
+++ projects/ion/src/lib/select/select.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'ion-select',
@@ -6,9 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select.component.css'],
 })
 export class IonSelectComponent implements OnInit {
+  @Input() disabledToggle = false;
+  @Input() showDropdown = true;
   placeholder = 'choose';
 
   ngOnInit(): void {
     this.placeholder = 'sada';
+  }
+
+  toggleDropdown(): void {
+    if (this.disabledToggle) {
+      this.showDropdown = true;
+      return;
+    }
+
+    this.showDropdown = !this.showDropdown;
   }
 }
--- projects/ion/src/lib/select/select.module.ts
+++ projects/ion/src/lib/select/select.module.ts
@@ -1,10 +1,11 @@
+import { IonInputModule } from './../input/input.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonSelectComponent } from './select.component';
 
 @NgModule({
   declarations: [IonSelectComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, IonInputModule],
   exports: [IonSelectComponent],
 })
 export class IonSelectModule {}
