d2345b5e49e2ebf369c5afa2653852d61aa849cb
Deeved Hiuston <deeved@gmail.com>
2023-04-04 20:21:41 -0300

    feat: closes the select when clicking outside the context of the element

--- projects/ion/src/lib/select/select.component.ts
+++ projects/ion/src/lib/select/select.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { SafeAny } from './../utils/safe-any';
+import { Component, Input, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'ion-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.css'],
 })
-export class IonSelectComponent {
+export class IonSelectComponent implements AfterViewInit {
   @Input() disabledToggle = false;
   @Input() showDropdown = false;
   @Input() placeholder = 'choose';
@@ -18,5 +19,22 @@ export class IonSelectComponent {
     }
 
     this.showDropdown = !this.showDropdown;
+  }
+
+  ngAfterViewInit(): void {
+    if (this.disabledToggle) {
+      return;
+    }
+
+    document.addEventListener('mouseup', (e: SafeAny) => {
+      const calendarContainer = document.getElementsByClassName(
+        'ion-select__container-dropdown'
+      )[0];
+      if (calendarContainer && !calendarContainer.contains(e.target)) {
+        setTimeout(() => {
+          this.showDropdown = false;
+        });
+      }
+    });
   }
 }
