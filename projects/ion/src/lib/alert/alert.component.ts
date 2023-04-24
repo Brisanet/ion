ba5b961cb8c9a95304c6bed40043321df16adb9d
Iury Nogueira <iunog@hotmail.com>
2023-04-24 15:16:54 -0300

    fix: ion-alert icon changes and background css fix

--- projects/ion/src/lib/alert/alert.component.ts
+++ projects/ion/src/lib/alert/alert.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { StatusType } from '../core/types';
 import { IconType } from '../core/types/icon';
 
@@ -14,7 +21,7 @@ const iconTypes = {
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.scss'],
 })
-export class IonAlertComponent implements OnInit {
+export class IonAlertComponent implements OnChanges {
   @Input() message!: string;
   @Input() type?: StatusType = 'success';
   @Input() closable? = false;
@@ -32,7 +39,7 @@ export class IonAlertComponent implements OnInit {
     this.iconType = iconTypes[this.type];
   }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
     this.setIcon();
   }
 }
