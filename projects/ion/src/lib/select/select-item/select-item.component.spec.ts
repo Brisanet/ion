a50c8dd3293131212052c96bf07d32f65ab2131c
Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
2023-05-29 10:19:16 -0300

    test: add test for ion-select-item-component

--- projects/ion/src/lib/select/select-item/select-item.component.spec.ts
+++ projects/ion/src/lib/select/select-item/select-item.component.spec.ts
@@ -1,27 +1,34 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { render, screen, fireEvent } from '@testing-library/angular';
+import { EventEmitter } from '@angular/core';
 import { IonSelectItemComponent } from './select-item.component';
 import { IonIconModule } from '../../icon/icon.module';
-
-describe('SelectItemComponent', () => {
-  let component: IonSelectItemComponent;
-  let fixture: ComponentFixture<IonSelectItemComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [IonSelectItemComponent],
-      imports: [IonIconModule],
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(IonSelectItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+import { SafeAny } from 'ion/lib/utils/safe-any';
+interface IonSelectItemProps {
+  label: string;
+  unselect?: EventEmitter<void>;
+}
+const sut = async (customProps?: IonSelectItemProps): Promise<void> => {
+  await render(IonSelectItemComponent, {
+    componentProperties: customProps,
+    imports: [IonIconModule],
+  });
+};
+describe('IonSelecItemComponent', () => {
+  it('should correctly render a label', async () => {
+    const customLabel = 'Option 01';
+    await sut({ label: customLabel });
+    expect(screen.getByText(customLabel)).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should emit an event when clicking of the icon', async () => {
+    const clickEvent = jest.fn();
+    await sut({
+      label: 'Option',
+      unselect: {
+        emit: clickEvent,
+      },
+    } as SafeAny);
+    fireEvent.click(screen.getByTestId('ion-icon-close'));
+    expect(clickEvent).toHaveBeenCalled();
   });
 });
