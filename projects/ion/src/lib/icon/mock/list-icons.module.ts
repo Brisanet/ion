1c761052aa4b20ee5e13f99526d9c94b18581eb6
Lucas <lucasemmanuelcs@gmail.com>
2023-06-05 10:21:45 -0300

    feat: list of icons refactored (#463) (#527)
    Co-authored-by: Iury Nogueira <iunog@hotmail.com>
    Co-authored-by: Larissa Cardoso <106749260+larissacard@users.noreply.github.com>
    Co-authored-by: Alysson Keysson <54484070+AlyssonMascarenhas@users.noreply.github.com>

---
+++ projects/ion/src/lib/icon/mock/list-icons.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonIconComponent } from '../icon.component';
+import { IonInputComponent } from '../../input/input.component';
+import { FormsModule } from '@angular/forms';
+import { IonNotificationComponent } from '../../notification/notification.component';
+
+@NgModule({
+  declarations: [IonInputComponent, IonIconComponent, IonNotificationComponent],
+  imports: [CommonModule, FormsModule],
+  exports: [IonInputComponent, IonIconComponent, IonNotificationComponent],
+})
+export class ListIconsMockModule {}
