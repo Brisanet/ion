6a28a6cf534b588eb7f98f41fed8198f2e3a392c
Allan <j.a.o.huthan@gmail.com>
2023-06-15 10:26:10 -0300

    test: test assertions updated to default props

--- projects/ion/src/lib/no-data/no-data.component.spec.ts
+++ projects/ion/src/lib/no-data/no-data.component.spec.ts
@@ -3,29 +3,40 @@ import { IonNoDataComponent } from './no-data.component';
 import { CommonModule } from '@angular/common';
 import { IonIconModule } from '../icon/icon.module';
 
-const defaultProps: IonNoDataComponent = {
-  label: 'Não há dados',
-  iconType: 'exclamation-rounded',
+const customLabel = 'No data';
+
+const testCustomProps: IonNoDataComponent = {
+  label: customLabel,
+  iconType: 'exclamation-solid',
 };
 
-const sut = async (
-  customProps: IonNoDataComponent = defaultProps
-): Promise<void> => {
+const sut = async (customProps?: IonNoDataComponent): Promise<void> => {
   await render(IonNoDataComponent, {
     componentProperties: customProps,
     imports: [CommonModule, IonIconModule],
   });
 };
 
-describe('IonNoDataComponent', () => {
+describe('IonNoDataComponent - default properties', () => {
   beforeEach(async () => {
     await sut();
   });
-  it('Should render the informed text', async () => {
+
+  it('Should render the default label when no label is informed', async () => {
     expect(screen.getByText('Não há dados')).toBeInTheDocument();
   });
 
-  it('Should render a icon when informed', async () => {
+  it('Should render a icon by default', async () => {
     expect(screen.getByTestId('ion-no-data-icon')).toBeInTheDocument();
+  });
+});
+
+describe('IonNoDataComponent - custom properties', () => {
+  beforeEach(async () => {
+    await sut(testCustomProps);
+  });
+
+  it('Should render the informed label', async () => {
+    expect(screen.getByText(customLabel)).toBeInTheDocument();
   });
 });
