0e8d5e9d2f371f90341c2c38b9cd337f9eeee0b8
Allan <j.a.o.huthan@gmail.com>
2023-07-14 08:01:50 -0300

    refactor: split conditions into functions

--- projects/ion/src/core/utils/clearObject.ts
+++ projects/ion/src/core/utils/clearObject.ts
@@ -3,21 +3,25 @@ import { isEmpty } from 'lodash';
 
 export function clearObject<T = SafeAny>(value: SafeAny): T {
   Object.keys(value).map((key) => {
-    const isNestedObj =
-      value[key] &&
-      !Array.isArray(value[key]) &&
-      typeof value[key] === 'object';
-
-    const shouldDelete =
-      [null, undefined, ''].includes(value[key]) ||
-      Number.isNaN(value[key]) ||
-      (Array.isArray(value[key]) && isEmpty(value[key]));
-
-    if (isNestedObj) {
+    if (isNestedObj(value[key])) {
       clearObject(value[key]);
-    } else if (shouldDelete) {
+    }
+
+    if (shouldDelete(value[key])) {
       delete value[key];
     }
   });
   return value;
+}
+
+function isNestedObj(value: SafeAny): boolean {
+  return value && !Array.isArray(value) && typeof value === 'object';
+}
+
+function shouldDelete(value: SafeAny): boolean {
+  return (
+    [null, undefined, ''].includes(value) ||
+    Number.isNaN(value) ||
+    (Array.isArray(value) && isEmpty(value))
+  );
 }
