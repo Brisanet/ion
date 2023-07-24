0fc238187bf582c67b1881f30769b64af106b4e6
Wérmeson Lopes <wermesonsl@outlook.com>
2023-07-24 13:55:42 -0300

    fix: atualizado para usar diretiva preexistente no Ion

--- projects/ion/src/lib/sidebar/sidebar.component.ts
+++ projects/ion/src/lib/sidebar/sidebar.component.ts
@@ -14,26 +14,8 @@ export class IonSidebarComponent {
 
   public closed = true;
 
-  public checkClikOnPage = (event): EventListenerOrEventListenerObject => {
-    const containerElement = document.querySelector('.ion-sidebar--opened');
-    const innerElement = event.target;
-    if (containerElement && !containerElement.contains(innerElement as Node)) {
-      const closeBUtton = document.querySelector(
-        '.ion-sidebar--opened .ion-sidebar__header button'
-      ) as HTMLElement;
-      closeBUtton.click();
-      document.removeEventListener('click', this.checkClikOnPage);
-    }
-    return;
-  };
-
   public toggleVisibility(): void {
     this.closed = !this.closed;
-    if (!this.closed) {
-      setTimeout(() => {
-        document.addEventListener('click', this.checkClikOnPage);
-      });
-    }
   }
 
   public itemSelected(itemIndex: number): void {
@@ -47,5 +29,9 @@ export class IonSidebarComponent {
   public groupSelected(groupIndex: number): void {
     unselectAllItems(this.items);
     callItemAction(this.items, groupIndex);
+  }
+
+  onClickOutside(): void {
+    this.closed = true;
   }
 }
--- projects/ion/src/lib/sidebar/sidebar.module.ts
+++ projects/ion/src/lib/sidebar/sidebar.module.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IonButtonModule } from '../button/button.module';
 import { IonIconModule } from '../icon/icon.module';
+import { IonSharedModule } from '../shared.module';
 import { IonSidebarGroupComponent } from './sidebar-group/sidebar-group.component';
 import { IonSidebarItemComponent } from './sidebar-item/sidebar-item.component';
 import { IonSidebarComponent } from './sidebar.component';
 
 @NgModule({
-  imports: [CommonModule, IonIconModule, IonButtonModule],
+  imports: [CommonModule, IonIconModule, IonButtonModule, IonSharedModule],
   declarations: [
     IonSidebarComponent,
     IonSidebarItemComponent,
