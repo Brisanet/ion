4ab73c5d7114e9f27740d042e567399aef942083
Madalena Campos <71613655+MadalenaCampos@users.noreply.github.com>
2023-07-28 15:40:46 -0300

    :memo: docs: add example img of custom row on table docs #738  (#750)
    * docs: add example img of custom row on table docs #738
    
    * feat: add required to chip-group #734
    
    * docs: add example img of custom row on table docs #738
    
    * fix: documentation problem #738
    
    ---------
    
    Co-authored-by: Iury Nogueira <iunog@hotmail.com>

--- stories/SmartTable.stories.ts
+++ stories/SmartTable.stories.ts
@@ -331,62 +331,6 @@ PopConfirmDynamicDescription.args = returnTableConfig(
   2
 );
 
-export const TableCustomRow = Template.bind({});
-TableCustomRow.args = {
-  config: {
-    columns,
-  },
-};
-
-TableCustomRow.parameters = {
-  docs: {
-    description: {
-      story: `Passos para customizar a linha da tabela.
-    1. No HTML do seu componente, crie um ng-template com a diretiva 'let-row' e realize as customizações desejadas.
-    A diretiva 'let-row' permite acessar os dados da linha através da identificação do objeto passado na configuração
-    da tabela. Veja o exemplo abaixo:
-
-    <ng-template #customTemplate let-row>
-      <td>{{row.id}}</td>
-      <td>{{ row.name }}</td>
-      <td><ion-icon [type]="row.active ? 'check' : 'close'"></ion-icon></td>
-      <td>
-        <ion-icon
-          type="info"
-          (click)="onDetails(row)"
-          style="cursor: pointer"
-        ></ion-icon>
-      </td>
-    </ng-template>
-
-    2. No arquivo .ts do seu componente, utilize o decorator '@ViewChild' para obter a referência do template customizado
-    criado no arquivo HTML.
-
-    export class AppComponent implements OnInit {
-      @ViewChild("customTemplate", { static: true })
-      customTemplate: TemplateRef<HTMLElement>;
-
-      ...
-    }
-
-    3. Passe a referência do template customizado para o atributo customRowTemplate da configuração da tabela.
-
-    export class AppComponent implements OnInit {
-      ...
-
-      ngOnInit(): void {
-        this.config = {
-          data,
-          columns,
-          customRowTemplate:  this.customTemplate,
-        }
-      }
-    }
-      `,
-    },
-  },
-};
-
 export const WithEllipsisOnCell = Template.bind({});
 WithEllipsisOnCell.args = returnTableConfig(
   longData,
@@ -396,4 +340,14 @@ WithEllipsisOnCell.args = returnTableConfig(
   2000,
   [10, 15, 30, 50, 100],
   true
+);
+
+export const WithTooltipInActions = Template.bind({});
+WithTooltipInActions.args = returnTableConfig(
+  data,
+  columns,
+  actions,
+  2,
+  2000,
+  [10, 15, 30, 50, 100]
 );
