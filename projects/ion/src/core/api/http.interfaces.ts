fd06c193fcb8c56397bc6d5928210c078761dc7d
Iury Nogueira <iunog@hotmail.com>
2023-08-24 14:44:17 -0300

    feat: add get method to bnService :sparkles: #803

--- projects/ion/src/core/api/http.interfaces.ts
+++ projects/ion/src/core/api/http.interfaces.ts
@@ -3,8 +3,9 @@ import { SmartPayload } from '../bn-table/bn-table';
 import { Observable } from 'rxjs';
 
 export interface BnService<DataType> {
-  list: (filters?: IPayload) => Observable<IResponse<DataType>>;
+  list?: (filters?: IPayload) => Observable<IResponse<DataType>>;
   insert?: (payload: IPayload) => Observable<SafeAny>;
+  get?: (item: number | string) => Observable<DataType>;
 }
 
 export interface IResponse<DataType> {
@@ -12,6 +13,7 @@ export interface IResponse<DataType> {
   dados?: DataType[];
   total?: number;
   mensagem?: string;
+  [x: string]: SafeAny;
 }
 
 export interface IPayload extends SmartPayload {
