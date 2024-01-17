05459dd924691869594045429ed46a87780bd303
“larissa-kamily-brisa” <larissa.kamily@grupobrisanet.com.br>
2024-01-17 13:07:37 -0300

    test: adding test cases

--- projects/ion/src/lib/utils/datePicker.spec.ts
+++ projects/ion/src/lib/utils/datePicker.spec.ts
@@ -1,25 +1,131 @@
 import { Day } from '../picker/core/day';
-import { isBetweenRange } from './datePicker';
+import {
+  getFormattedDate,
+  getInitialDate,
+  isBetweenRange,
+  isSameDate,
+  isSameDay,
+  isToday,
+} from './datePicker';
 
-const selectedDays = [
-  new Day(new Date('2024-01-01')),
-  new Day(new Date('2024-01-10')),
-];
+const today = new Date('2024-01-10T03:00:00.000Z');
+jest.useFakeTimers('modern').setSystemTime(today);
 
-describe('isBetweenRange', () => {
-  it('should return true to dates between a range', () => {
-    const currentDay = new Day(new Date('2024-01-05'));
+describe('DatePicker', () => {
+  let result: boolean;
+  describe('isToday', () => {
+    let currentDate: Day;
+    let lang: string;
 
-    const result = isBetweenRange(currentDay, selectedDays);
+    it('should return true if day is today', () => {
+      currentDate = new Day(today);
+      lang = 'pt-BR';
 
-    expect(result).toBe(true);
+      result = isToday(currentDate, lang);
+
+      expect(result).toBe(true);
+    });
+    it('should return false if day is not today', () => {
+      currentDate = new Day(new Date('2024-01-03'));
+      lang = 'pt-BR';
+
+      result = isToday(currentDate, lang);
+
+      expect(result).toBe(false);
+    });
   });
 
-  it('should return false to dates that are not within a range', () => {
-    const currentDay = new Day(new Date('2024-02-01'));
+  describe('isSameDay', () => {
+    let day: Day;
+    let dayToCompare: Day;
 
-    const result = isBetweenRange(currentDay, selectedDays);
+    it('should return true if days are equal', () => {
+      day = new Day(new Date('2024-01-01'));
+      dayToCompare = new Day(new Date('2024-01-01'));
 
-    expect(result).toBe(false);
+      result = isSameDay(day, dayToCompare);
+
+      expect(result).toBe(true);
+    });
+    it('should return false if day arent equal', () => {
+      day = new Day(new Date('2024-01-01'));
+      dayToCompare = new Day(new Date('2024-01-02'));
+
+      result = isSameDay(day, dayToCompare);
+
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('isSameDate', () => {
+    let initial_date: Date;
+    let final_date: Date;
+
+    it('should return false if dates are equal', () => {
+      initial_date = new Date('2024-01-01');
+      final_date = new Date('2024-01-01');
+
+      result = isSameDate(initial_date, final_date);
+
+      expect(result).toBe(false);
+    });
+    it('should return true if dates arent equal', () => {
+      initial_date = new Date('2024-01-01');
+      final_date = new Date('2024-04-01');
+
+      result = isSameDate(initial_date, final_date);
+
+      expect(result).toBe(true);
+    });
+  });
+
+  describe('getInitialDate', () => {
+    it('should return formatted data when current data is provided', () => {
+      const currentDate = ['2024-01-10'];
+      const result = getInitialDate(currentDate);
+
+      expect(result).toEqual(new Date());
+    });
+
+    it('should return current date when no date is given', () => {
+      const result = getInitialDate([]);
+
+      expect(result).toEqual(new Date());
+    });
+  });
+
+  describe('getFormattedDate', () => {
+    it('should return the date formatted as expected', () => {
+      const dates = ['2024-01-10'];
+      const isFinalOfRange = false;
+
+      const formattedDate = getFormattedDate(dates, isFinalOfRange);
+
+      expect(formattedDate).toEqual(new Date());
+    });
+  });
+
+  describe('isBetweenRange', () => {
+    let currentDay: Day;
+
+    const selectedDays = [
+      new Day(new Date('2024-01-01')),
+      new Day(new Date('2024-01-10')),
+    ];
+
+    it('should return true to dates between a range', () => {
+      currentDay = new Day(new Date('2024-01-05'));
+
+      result = isBetweenRange(currentDay, selectedDays);
+
+      expect(result).toBe(true);
+    });
+    it('should return false to dates that are not within a range', () => {
+      currentDay = new Day(new Date('2024-02-01'));
+
+      result = isBetweenRange(currentDay, selectedDays);
+
+      expect(result).toBe(false);
+    });
   });
 });
