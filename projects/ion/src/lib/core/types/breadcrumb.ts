8d4292a28645e638bed162340e845766de40a713
vinicius-guedes-brisa <vinicius.guedes@grupobrisanet.com.br>
2024-02-26 15:40:49 -0300

    refactor: remove unused code

--- projects/ion/src/lib/core/types/breadcrumb.ts
+++ projects/ion/src/lib/core/types/breadcrumb.ts
@@ -1,11 +1,3 @@
-import { EventEmitter } from '@angular/core';
-
-export interface BreadcrumbProps {
-  breadcrumbItems: BreadcrumbItem[];
-  truncate?: boolean;
-  selected: EventEmitter<BreadcrumbItem>;
-}
-
 export interface BreadcrumbItem {
   label: string;
   link: string;
