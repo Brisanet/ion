4475203d838b9148d2f66dea5b3de2f855ce465a
Vinicius Guedes <138119077+vinicius-guedes-brisa@users.noreply.github.com>
2024-02-29 17:26:32 -0300

    feat: create truncation in breadcrumbs (#1034)
    * feat: create truncation in breadcrumbs
    
    * refactor: adjust conditions
    
    * refactor: remove unused code
    
    * fix: breadcrumb component imports
    
    * fix: breadcrumb component imports
    
    * refactor: create control to ellipsesIndex
    
    * fix: adjust storybook improve template logic
    
    * refactor: remove unused import

--- projects/ion/src/lib/breadcrumb/breadcrumb.module.ts
+++ projects/ion/src/lib/breadcrumb/breadcrumb.module.ts
@@ -1,11 +1,13 @@
-import { IonIconModule } from './../icon/icon.module';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { IonDropdownModule } from '../dropdown/dropdown.module';
+import { IonIconModule } from './../icon/icon.module';
 import { IonBreadcrumbComponent } from './breadcrumb.component';
 
 @NgModule({
   declarations: [IonBreadcrumbComponent],
-  imports: [CommonModule, IonIconModule],
+  imports: [CommonModule, IonIconModule, IonDropdownModule],
   exports: [IonBreadcrumbComponent],
 })
 export class IonBreadcrumbModule {}
