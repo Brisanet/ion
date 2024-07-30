a69cf543d57d63f50a8d2d6da66a1d47589532f9
vinicius-guedes-brisa <vinicius.guedes@grupobrisanet.com.br>
2024-07-30 16:11:38 -0300

    refactor: position calculator callback

--- projects/ion/src/lib/tour/tour-position.calculator.ts
+++ projects/ion/src/lib/tour/tour-position.calculator.ts
@@ -9,15 +9,6 @@ type GetPositionsCallback = (
   props: GetPositionsCallbackProps
 ) => PopoverPositions;
 
-interface PositionParams {
-  host: DOMRect;
-  popover: DOMRect;
-  arrowAtCenter: boolean;
-  hostHorizontalCenter: number;
-  hostVerticalCenter: number;
-  marginToContent: number;
-}
-
 export function generatePositionCallback(
   contentPadding: number,
   marginToContent: number
@@ -30,84 +21,68 @@ export function getPositionsPopover(
   contentPadding: number,
   marginToContent: number
 ): PopoverPositions {
-  const { host, element: popover } = props;
-  const hostHorizontalCenter = Math.round(host.width / 2 + host.left);
-  const hostVerticalCenter = Math.round(host.height / 2 + host.top);
-  const calculatePositionProps = {
-    host: {
-      top: host.top - contentPadding,
-      bottom: host.bottom + contentPadding,
-      left: host.left - contentPadding,
-      right: host.right + contentPadding,
-      width: host.width + contentPadding * 2,
-      height: host.height + contentPadding * 2,
+  const { host: rawHost, element: popover } = props;
+
+  const hostHorizontalCenter = Math.round(rawHost.width / 2 + rawHost.left);
+  const hostVerticalCenter = Math.round(rawHost.height / 2 + rawHost.top);
+
+  const host = {
+    top: rawHost.top - contentPadding,
+    bottom: rawHost.bottom + contentPadding,
+    left: rawHost.left - contentPadding,
+    right: rawHost.right + contentPadding,
+    width: rawHost.width + contentPadding * 2,
+    height: rawHost.height + contentPadding * 2,
+  };
+
+  return {
+    topRight: {
+      top: host.top - popover.height - marginToContent,
+      left: host.right - popover.width,
     },
-    popover,
-    hostHorizontalCenter,
-    hostVerticalCenter,
-    marginToContent,
-  } as PositionParams;
-
-  return {
-    ...calculateTopPositions(calculatePositionProps),
-    ...calculateBottomPositions(calculatePositionProps),
-    ...calculateLeftPositions(calculatePositionProps),
-    ...calculateRightPositions(calculatePositionProps),
-  } as PopoverPositions;
-}
-
-function calculateTopPositions({
-  host,
-  popover,
-  marginToContent,
-  hostHorizontalCenter,
-}: PositionParams): Partial<PopoverPositions> {
-  const top = host.top - popover.height - marginToContent;
-  return {
-    topRight: { left: host.right - popover.width, top },
-    topCenter: { left: hostHorizontalCenter - popover.width / 2, top },
-    topLeft: { left: host.left, top },
-  };
-}
-
-function calculateBottomPositions({
-  host,
-  popover,
-  marginToContent,
-  hostHorizontalCenter,
-}: PositionParams): Partial<PopoverPositions> {
-  const top = host.bottom + marginToContent;
-  return {
-    bottomRight: { left: host.right - popover.width, top },
-    bottomCenter: { left: hostHorizontalCenter - popover.width / 2, top },
-    bottomLeft: { left: host.left, top },
-  };
-}
-
-function calculateLeftPositions({
-  host,
-  popover,
-  marginToContent,
-  hostVerticalCenter,
-}: PositionParams): Partial<PopoverPositions> {
-  const left = host.left - popover.width - marginToContent;
-  return {
-    leftBottom: { left, top: host.bottom - popover.height },
-    leftCenter: { left, top: hostVerticalCenter - popover.height / 2 },
-    leftTop: { left, top: host.top },
-  };
-}
-
-function calculateRightPositions({
-  host,
-  popover,
-  marginToContent,
-  hostVerticalCenter,
-}: PositionParams): Partial<PopoverPositions> {
-  const left = host.right + marginToContent;
-  return {
-    rightBottom: { left, top: host.bottom - popover.height },
-    rightCenter: { left, top: hostVerticalCenter - popover.height / 2 },
-    rightTop: { left, top: host.top },
+    topCenter: {
+      top: host.top - popover.height - marginToContent,
+      left: hostHorizontalCenter - popover.width / 2,
+    },
+    topLeft: {
+      top: host.top - popover.height - marginToContent,
+      left: host.left,
+    },
+    bottomRight: {
+      top: host.bottom + marginToContent,
+      left: host.right - popover.width,
+    },
+    bottomCenter: {
+      top: host.bottom + marginToContent,
+      left: hostHorizontalCenter - popover.width / 2,
+    },
+    bottomLeft: {
+      top: host.bottom + marginToContent,
+      left: host.left,
+    },
+    leftBottom: {
+      left: host.left - popover.width - marginToContent,
+      top: host.bottom - popover.height,
+    },
+    leftCenter: {
+      left: host.left - popover.width - marginToContent,
+      top: hostVerticalCenter - popover.height / 2,
+    },
+    leftTop: {
+      left: host.left - popover.width - marginToContent,
+      top: host.top,
+    },
+    rightBottom: {
+      left: host.right + marginToContent,
+      top: host.bottom - popover.height,
+    },
+    rightCenter: {
+      left: host.right + marginToContent,
+      top: hostVerticalCenter - popover.height / 2,
+    },
+    rightTop: {
+      left: host.right + marginToContent,
+      top: host.top,
+    },
   };
 }
