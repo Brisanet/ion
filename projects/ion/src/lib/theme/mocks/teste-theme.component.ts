500af34ffaa3d6685583def1c121ec87b53fc4a0
vinicius-guedes-brisa <vinicius.guedes@grupobrisanet.com.br>
2024-09-03 17:32:13 -0300

    feat: migrating button to ion-theme v1

--- projects/ion/src/lib/theme/mocks/teste-theme.component.ts
+++ projects/ion/src/lib/theme/mocks/teste-theme.component.ts
@@ -6,17 +6,48 @@ import { IonThemes, IonThemeService } from '../theme.service';
   template: `
     <main>
       <div>
-        <button (click)="setTheme(ionThemes.LIGHT)">Tema claro</button>
-        <button (click)="setTheme(ionThemes.DARK)">Tema escuro</button>
+        <ion-button
+          label="Tema claro"
+          type="secondary"
+          (ionOnClick)="setTheme(ionThemes.LIGHT)"
+        ></ion-button>
+        <ion-button
+          label="Tema escuro"
+          type="secondary"
+          (ionOnClick)="setTheme(ionThemes.DARK)"
+        ></ion-button>
       </div>
 
       <ion-alert message="oi"></ion-alert>
+
+      <table>
+        <tr *ngFor="let size of sizeOptions">
+          <td *ngFor="let variant of variantOptions">
+            <ion-button
+              [label]="variant + ' ' + size"
+              [type]="variant"
+              [size]="size"
+              [danger]="danger"
+              [loading]="loading"
+              [disabled]="true"
+            ></ion-button>
+          </td>
+        </tr>
+      </table>
     </main>
   `,
   styleUrls: ['./teste-theme.component.scss'],
 })
 export class TesteThemeComponent {
   public ionThemes = IonThemes;
+
+  danger = false;
+  loading = false;
+  disabled = true;
+
+  sizeOptions = ['sm', 'md', 'lg', 'xl'];
+
+  variantOptions = ['primary', 'secondary', 'ghost', 'dashed'];
 
   constructor(private readonly ionThemeService: IonThemeService) {}
 
--- stories/Theme.stories.ts
+++ stories/Theme.stories.ts
@@ -1,11 +1,14 @@
 import { Meta, Story } from '@storybook/angular';
 import { TesteThemeComponent } from '../projects/ion/src/lib/theme/mocks/teste-theme.component';
-import { IonAlertModule } from '../projects/ion/src/public-api';
+import {
+  IonAlertModule,
+  IonSharedModule,
+} from '../projects/ion/src/public-api';
 
 const Template: Story<TesteThemeComponent> = () => ({
   component: TesteThemeComponent,
   moduleMetadata: {
-    imports: [IonAlertModule],
+    imports: [IonAlertModule, IonSharedModule],
     declarations: [TesteThemeComponent],
   },
 });
