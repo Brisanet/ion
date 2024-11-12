14ed8fb098c2c0bb338893cfa9a903b5a74303b3
allan-chagas-brisa <allan.chagas@grupobrisanet.com.br>
2024-11-12 17:03:08 -0300

    fix: loading on ion button with dropdown

--- projects/ion/src/lib/button/button.component.spec.ts
+++ projects/ion/src/lib/button/button.component.spec.ts
@@ -289,6 +289,20 @@ describe('ButtonComponent with dropdown', () => {
       userEvent.type(screen.getByTestId('input-element'), typeText);
       expect(searchEvent).toHaveBeenLastCalledWith(typeText);
     });
+
+    it('should not render the dropdown arrow when loading', async () => {
+      const options = [{ label: 'Option 1' }, { label: 'Option 2' }];
+
+      await sut({
+        label: defaultName,
+        loading: true,
+        options,
+      });
+
+      const arrowIcon = document.getElementById('ion-icon-semi-down');
+
+      expect(arrowIcon).toBeFalsy();
+    });
   });
 
   it('should close dropdown when click outside component', async () => {
