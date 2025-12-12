0c7c233c96219ec151e3ccac90bb310ecbe0aec7
Jairo Chaves <jairochaves2@gmail.com>
2025-12-12 17:27:26 -0300

    feat: add no-data (#1294)
    * feat: add no-data

---
+++ projects/ion/src/lib/no-data/no-data.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { IonIconComponent } from '../icon/icon.component';
+import { IconType } from '../core/types';
+
+@Component({
+  selector: 'ion-no-data',
+  standalone: true,
+  imports: [CommonModule, IonIconComponent],
+  templateUrl: './no-data.component.html',
+  styleUrls: ['./no-data.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class IonNoDataComponent {
+  iconType = input<IconType>('exclamation-rounded');
+  label = input<string>('Não há dados');
+}
