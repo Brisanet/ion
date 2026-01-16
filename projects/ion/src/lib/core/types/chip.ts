15e702034003df37293312cbfb544b4c7a6a08cb
Jairo Chaves <138058372+jairo-chaves-brisa@users.noreply.github.com>
2026-01-16 13:38:18 -0300

    Add chip badge (#1324)
    * fix: add position at badge of chip
    
    * fix: remove circular dependecy
    
    ---------
    
    Co-authored-by: Jairo Chaves <jairo.chaves@grupobrisanet.com>

--- projects/ion/src/lib/core/types/chip.ts
+++ projects/ion/src/lib/core/types/chip.ts
@@ -1,5 +1,5 @@
 import { EventEmitter } from '@angular/core';
-import { BadgeType } from './badge';
+import { BadgeProps } from './badge';
 import { DropdownItem, DropdownParams } from './dropdown';
 import { IconDirection } from './icon';
 import { InfoBadgeStatus } from './info-badge';
@@ -8,9 +8,8 @@ export interface Badge {
   value: number;
 }
 
-export interface RightBadge {
-  label: string;
-  type: BadgeType;
+export interface UserBadge extends BadgeProps {
+  position?: 'left' | 'right';
 }
 
 export type ChipSize = 'sm' | 'md';
@@ -32,8 +31,9 @@ export interface IonChipProps {
   multiple?: boolean;
   required?: boolean;
   infoBadge?: InfoBadgeStatus;
+  showToggle?: boolean;
   iconPosition?: IconDirection;
-  rightBadge?: RightBadge;
+  badge?: UserBadge;
   dropdownEvents?: EventEmitter<DropdownItem[]>;
   dropdownSearchConfig?: Pick<DropdownParams, 'searchOptions' | 'enableSearch'>;
   dropdownSearchEvents?: EventEmitter<string>;
